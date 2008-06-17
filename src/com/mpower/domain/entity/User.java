20d819f79311f30c82b5954766c7f66d85e36b96
jball <jball@0f581344-5646-3a48-8ded-616bfea576c6>
2008-06-17 14:44:54 +0000

    set empty strings to null in db
    git-svn-id: svn://svn.orangeleap.com/tangerine/mpower/trunk@1082 0f581344-5646-3a48-8ded-616bfea576c6

--- src/com/mpower/domain/entity/User.java
+++ src/com/mpower/domain/entity/User.java
@@ -4,62 +4,66 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.mpower.domain.entity.listener.EmptyStringNullifyerListener;
+
 @Entity
-@Table(name="MPOWER_USER")
+@EntityListeners(value = { EmptyStringNullifyerListener.class })
+@Table(name = "MPOWER_USER")
 public class User implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue
-	@Column(name="USER_ID")
-	private Long id;
+    @Id
+    @GeneratedValue
+    @Column(name = "USER_ID")
+    private Long id;
 
-	@Column(name="USER_NAME")
-	private String userName;
+    @Column(name = "USER_NAME")
+    private String userName;
 
-	@Column(name="PASSWORD")
-	private String password;
+    @Column(name = "PASSWORD")
+    private String password;
 
-	@ManyToOne
-	@JoinColumn(name="SITE_ID")
-	private Site site;
+    @ManyToOne
+    @JoinColumn(name = "SITE_ID")
+    private Site site;
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getUserName() {
-		return userName;
-	}
+    public String getUserName() {
+        return userName;
+    }
 
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public Site getSite() {
-		return site;
-	}
+    public Site getSite() {
+        return site;
+    }
 
-	public void setSite(Site site) {
-		this.site = site;
-	}
+    public void setSite(Site site) {
+        this.site = site;
+    }
 }

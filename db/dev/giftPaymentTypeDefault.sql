604fa70ae4f23f6e4d1203bed72f87b010c49d80
jball <jball@0f581344-5646-3a48-8ded-616bfea576c6>
2008-06-18 21:47:15 +0000

    honor default value for gift
    git-svn-id: svn://svn.orangeleap.com/tangerine/mpower/trunk@1121 0f581344-5646-3a48-8ded-616bfea576c6

--- db/dev/giftPaymentTypeDefault.sql
+++ db/dev/giftPaymentTypeDefault.sql
@@ -1,3 +1,3 @@
--- make person's custom field, dogName, default to Spot
+-- make gift payment source's payment type default to Cash
 
 INSERT INTO ENTITY_DEFAULT (SITE_ID, ENTITY_TYPE, ENTITY_FIELD_NAME, DEFAULT_VALUE) VALUES (1, 'gift', 'paymentSource.paymentType', 'Cash');

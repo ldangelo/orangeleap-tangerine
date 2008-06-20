bdfb5061cb013d858d8afe2635e48f0c5cdeb9fe
jball <jball@0f581344-5646-3a48-8ded-616bfea576c6>
2008-06-20 20:31:56 +0000

    Encryption for credit card information
    git-svn-id: svn://svn.orangeleap.com/tangerine/mpower/trunk@1157 0f581344-5646-3a48-8ded-616bfea576c6

---
+++ src/com/mpower/domain/util/AESException.java
@@ -0,0 +1,18 @@
+package com.mpower.domain.util;
+
+public class AESException extends RuntimeException {
+
+    /**
+     * Serial Version ID
+     */
+    private static final long serialVersionUID = 1L;
+
+    public AESException(String msg) {
+        super(msg);
+    }
+
+    public AESException(String msg, Throwable root) {
+        super(msg, root);
+    }
+
+}

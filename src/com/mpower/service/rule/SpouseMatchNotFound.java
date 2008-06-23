713fb3f1e2a5794a173f4a3b26c34b537fce32de
jfridye <jfridye@0f581344-5646-3a48-8ded-616bfea576c6>
2008-06-23 21:34:40 +0000

    updated matching spouse rule
    git-svn-id: svn://svn.orangeleap.com/tangerine/mpower/trunk@1169 0f581344-5646-3a48-8ded-616bfea576c6

---
+++ src/com/mpower/service/rule/SpouseMatchNotFound.java
@@ -0,0 +1,17 @@
+package com.mpower.service.rule;
+
+public class SpouseMatchNotFound {
+	private boolean found;
+
+	public SpouseMatchNotFound(boolean found) {
+		this.found = found;
+	}
+	public boolean isFound() {
+		return found;
+	}
+
+	public void setFound(boolean found) {
+		this.found = found;
+	}
+	
+}

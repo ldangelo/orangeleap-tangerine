189a055c4e10f4773aa89e09126d81e571c6994e
rsmith <rsmith@0f581344-5646-3a48-8ded-616bfea576c6>
2008-06-24 18:46:53 +0000

    person validation
    git-svn-id: svn://svn.orangeleap.com/tangerine/mpower/trunk@1174 0f581344-5646-3a48-8ded-616bfea576c6

--- src/com/mpower/controller/validator/PersonValidator.java
+++ src/com/mpower/controller/validator/PersonValidator.java
@@ -10,26 +10,26 @@ import com.mpower.domain.entity.Person;
 
 public class PersonValidator implements Validator {
 
-    /** Logger for this class and subclasses */
-    protected final Log logger = LogFactory.getLog(getClass());
+	/** Logger for this class and subclasses */
+	protected final Log logger = LogFactory.getLog(getClass());
 
-    private String pageName;
+	private String pageName;
 
-    /* This is not implemented yet.  Right now we have static validation rules
-     * in place.  We need a PageCustomizationService that will allow for doing
-     * dynamic validation
-     */
-    /*private PageCustomizationService pageCustomizationService;
-
-	public PageCustomizationService getPageCustomizationService() {
-		return pageCustomizationService;
-	}
-
-	public void setPageCustomizationService(
-			PageCustomizationService pageCustomizationService) {
-		this.pageCustomizationService = pageCustomizationService;
-	}
-*/
+	/*
+	 * This is not implemented yet. Right now we have static validation rules in
+	 * place. We need a PageCustomizationService that will allow for doing
+	 * dynamic validation
+	 */
+	/*
+	 * private PageCustomizationService pageCustomizationService;
+	 * 
+	 * public PageCustomizationService getPageCustomizationService() { return
+	 * pageCustomizationService; }
+	 * 
+	 * public void setPageCustomizationService( PageCustomizationService
+	 * pageCustomizationService) { this.pageCustomizationService =
+	 * pageCustomizationService; }
+	 */
 	public String getPageName() {
 		return pageName;
 	}
@@ -39,7 +39,7 @@ public class PersonValidator implements Validator {
 	}
 
 	@SuppressWarnings("unchecked")
-    @Override
+	@Override
 	public boolean supports(Class clazz) {
 		// TODO Auto-generated method stub
 		return Person.class.equals(clazz);
@@ -51,22 +51,23 @@ public class PersonValidator implements Validator {
 		logger.info("**** Validating Person from Save person form");
 
 		@SuppressWarnings("unused")
-        Person person = (Person) target;
+		Person person = (Person) target;
 		// get person then get site
 
 		// This is an example of validating that the lastName field in person
-		// is not empty, null, or whitespace.  If it is, we grab the empty msg
+		// is not empty, null, or whitespace. If it is, we grab the empty msg
 		// from message.properties.
-		//ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lastName", "empty.lastName");
+		// ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lastName",
+		// "empty.lastName");
 
 		// This is a different form of validation, very similar to the one above
 		// but if the 3rd arg is not found in our message.properties, we use
 		// 4th param as a default message.
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lastName", "empty.lastName",
-				"default message");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lastName",
+				"empty.lastName", "default message");
 
 		// this is for working towards completely dynamic validation.
-		//errors.rejectValue(field, errorCode, errorArgs, defaultMessage)
+		// errors.rejectValue(field, errorCode, errorArgs, defaultMessage)
 	}
 
 }

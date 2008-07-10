a6a1575099008f13ec6acba3c972b3dbff0a6b8d
jball <jball@0f581344-5646-3a48-8ded-616bfea576c6>
2008-07-10 15:12:32 +0000

    checkin initial db security
    git-svn-id: svn://svn.orangeleap.com/tangerine/mpower/trunk@1376 0f581344-5646-3a48-8ded-616bfea576c6

--- src/com/mpower/service/UserService.java
+++ src/com/mpower/service/UserService.java
@@ -1,11 +1,16 @@
 package com.mpower.service;
 
+import org.springframework.dao.DataAccessException;
+import org.springframework.security.userdetails.UserDetails;
+import org.springframework.security.userdetails.UsernameNotFoundException;
+
 import com.mpower.domain.User;
 
 public interface UserService {
 
 	public User authenticateUser(String userName, String password);
-	
+
 	public User maintainUser(User user);
-	
+
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException, DataAccessException;
 }

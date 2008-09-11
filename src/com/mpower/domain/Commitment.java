8b8bf229c5fcb8df3b9efac167f5a41a1dec4994
jball <jball@0f581344-5646-3a48-8ded-616bfea576c6>
2008-09-11 21:58:11 +0000

    add unique constraint enforcing commitment name/person id unique
    git-svn-id: svn://svn.orangeleap.com/tangerine/mpower/trunk@2023 0f581344-5646-3a48-8ded-616bfea576c6

--- src/com/mpower/domain/Commitment.java
+++ src/com/mpower/domain/Commitment.java
@@ -23,6 +23,7 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Transient;
+import javax.persistence.UniqueConstraint;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -34,297 +35,297 @@ import com.mpower.util.CommitmentCustomFieldMap;
 
 @Entity
 @EntityListeners(value = { TemporalTimestampListener.class })
-@Table(name = "COMMITMENT")
+@Table(name = "COMMITMENT", uniqueConstraints = @UniqueConstraint(columnNames = { "NAME", "PERSON_ID" }))
 public class Commitment implements Customizable, Viewable, Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@SuppressWarnings("unused")
-	@Transient
-	private final Log logger = LogFactory.getLog(getClass());
+    @SuppressWarnings("unused")
+    @Transient
+    private final Log logger = LogFactory.getLog(getClass());
 
-	@Id
-	@GeneratedValue
-	@Column(name = "COMMITMENT_ID")
-	private Long id;
+    @Id
+    @GeneratedValue
+    @Column(name = "COMMITMENT_ID")
+    private Long id;
 
-	@ManyToOne
-	@JoinColumn(name = "PERSON_ID")
-	private Person person;
+    @ManyToOne
+    @JoinColumn(name = "PERSON_ID")
+    private Person person;
 
-	@OneToMany(mappedBy = "commitment")
-	private List<Gift> gifts;
+    @OneToMany(mappedBy = "commitment")
+    private List<Gift> gifts;
 
-	@Column(name = "NAME")
-	private String name;
+    @Column(name = "NAME")
+    private String name;
 
-	@Column(name = "COMMENTS")
-	private String comments;
+    @Column(name = "COMMENTS")
+    private String comments;
 
-	@Column(name = "AMOUNT_PER_GIFT")
-	private BigDecimal amountPerGift;
+    @Column(name = "AMOUNT_PER_GIFT")
+    private BigDecimal amountPerGift;
 
-	@Column(name = "NUMBER_OF_GIFTS")
-	private Integer numberOfGifts;
+    @Column(name = "NUMBER_OF_GIFTS")
+    private Integer numberOfGifts;
 
-	@Column(name = "COMMITMENT_CODE")
-	private String commitmentCode;
+    @Column(name = "COMMITMENT_CODE")
+    private String commitmentCode;
 
-	@Column(name = "PROJECT_CODE")
-	private String projectCode;
+    @Column(name = "PROJECT_CODE")
+    private String projectCode;
 
-	@Column(name = "MOTIVATION_CODE")
-	private String motivationCode;
+    @Column(name = "MOTIVATION_CODE")
+    private String motivationCode;
 
-	@OneToMany(mappedBy = "commitment", cascade = CascadeType.ALL)
-	private List<CommitmentCustomField> commitmentCustomFields;
+    @OneToMany(mappedBy = "commitment", cascade = CascadeType.ALL)
+    private List<CommitmentCustomField> commitmentCustomFields;
 
-	@Column(name = "START_DATE")
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date startDate;
+    @Column(name = "START_DATE")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date startDate;
 
-	@Column(name = "END_DATE")
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date endDate;
+    @Column(name = "END_DATE")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date endDate;
 
-	@Column(name = "STATUS_TYPE")
-	@Enumerated(EnumType.STRING)
-	private CommitmentStatusType statusType;
+    @Column(name = "STATUS_TYPE")
+    @Enumerated(EnumType.STRING)
+    private CommitmentStatusType statusType;
 
-	@Column(name = "CREATE_DATE", updatable = false)
-	@Temporal(TemporalType.TIMESTAMP)
-	@AutoPopulate
-	private Date createDate;
+    @Column(name = "CREATE_DATE", updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @AutoPopulate
+    private Date createDate;
 
-	@Column(name = "UPDATE_DATE", updatable = true)
-	@Temporal(TemporalType.TIMESTAMP)
-	@AutoPopulate
-	private Date updateDate;
+    @Column(name = "UPDATE_DATE", updatable = true)
+    @Temporal(TemporalType.TIMESTAMP)
+    @AutoPopulate
+    private Date updateDate;
 
-	@Column(name = "DEDUCTIBLE")
-	private boolean deductible = false;
+    @Column(name = "DEDUCTIBLE")
+    private boolean deductible = false;
 
-	@Column(name = "AUTO_PAY")
-	private boolean autoPay = false;
+    @Column(name = "AUTO_PAY")
+    private boolean autoPay = false;
 
-	@Column(name = "NOTES")
-	private String notes;
+    @Column(name = "NOTES")
+    private String notes;
 
-	@Transient
-	private Map<String, CustomField> customFieldMap = null;
+    @Transient
+    private Map<String, CustomField> customFieldMap = null;
 
-	@Transient
-	private Set<String> requiredFields = null;
+    @Transient
+    private Set<String> requiredFields = null;
 
-	@Transient
-	private Map<String, String> validationMap = null;
-
-	@Transient
-	private Map<String, String> fieldLabelMap = null;
+    @Transient
+    private Map<String, String> validationMap = null;
+
+    @Transient
+    private Map<String, String> fieldLabelMap = null;
 
-	@Transient
-	private Map<String, Object> fieldValueMap = null;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Person getPerson() {
-		return person;
-	}
-
-	public void setPerson(Person person) {
-		this.person = person;
-	}
-
-	public List<Gift> getGifts() {
-		return gifts;
-	}
-
-	public void setGifts(List<Gift> gifts) {
-		this.gifts = gifts;
-	}
-
-	public void setComments(String comments) {
-		this.comments = comments;
-	}
-
-	public BigDecimal getAmountPerGift() {
-		return amountPerGift;
-	}
-
-	public void setAmountPerGift(BigDecimal amountPerGift) {
-		this.amountPerGift = amountPerGift;
-	}
-
-	public Integer getNumberOfGifts() {
-		return numberOfGifts;
-	}
-
-	public void setNumberOfGifts(Integer numberOfGifts) {
-		this.numberOfGifts = numberOfGifts;
-	}
-
-	public List<CommitmentCustomField> getCustomFields() {
-		if (commitmentCustomFields == null) {
-			commitmentCustomFields = new ArrayList<CommitmentCustomField>();
-		}
-		return commitmentCustomFields;
-	}
-
-	@SuppressWarnings("unchecked")
-	public Map<String, CustomField> getCustomFieldMap() {
-		if (customFieldMap == null) {
-			customFieldMap = CommitmentCustomFieldMap.buildCustomFieldMap(getCustomFields(), this);
-		}
-		return customFieldMap;
-	}
-
-	@Override
-	public Set<String> getRequiredFields() {
-		return requiredFields;
-	}
-
-	@Override
-	public void setRequiredFields(Set<String> requiredFields) {
-		this.requiredFields = requiredFields;
-	}
-
-	@Override
-	public Map<String, String> getValidationMap() {
-		return validationMap;
-	}
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getComments() {
-		return comments;
-	}
-
-	@Override
-	public void setValidationMap(Map<String, String> validationMap) {
-		this.validationMap = validationMap;
-	}
-
-	@Override
-	public Map<String, String> getFieldLabelMap() {
-		return fieldLabelMap;
-	}
-
-	@Override
-	public void setFieldLabelMap(Map<String, String> fieldLabelMap) {
-		this.fieldLabelMap = fieldLabelMap;
-	}
-
-	@Override
-	public Map<String, Object> getFieldValueMap() {
-		return fieldValueMap;
-	}
-
-	@Override
-	public void setFieldValueMap(Map<String, Object> fieldValueMap) {
-		this.fieldValueMap = fieldValueMap;
-	}
-
-	@Override
-	public Site getSite() {
-		return person != null ? person.getSite() : null;
-	}
-
-	public String getCommitmentCode() {
-		return commitmentCode;
-	}
-
-	public void setCommitmentCode(String commitmentCode) {
-		this.commitmentCode = commitmentCode;
-	}
-
-	public String getProjectCode() {
-		return projectCode;
-	}
-
-	public void setProjectCode(String projectCode) {
-		this.projectCode = projectCode;
-	}
-
-	public String getMotivationCode() {
-		return motivationCode;
-	}
-
-	public void setMotivationCode(String motivationCode) {
-		this.motivationCode = motivationCode;
-	}
-
-	public Date getStartDate() {
-		return startDate;
-	}
-
-	public void setStartDate(Date startDate) {
-		this.startDate = startDate;
-	}
-
-	public Date getEndDate() {
-		return endDate;
-	}
-
-	public void setEndDate(Date endDate) {
-		this.endDate = endDate;
-	}
-
-	public CommitmentStatusType getStatusType() {
-		return statusType;
-	}
-
-	public void setStatusType(CommitmentStatusType statusType) {
-		this.statusType = statusType;
-	}
-
-	public Date getCreateDate() {
-		return createDate;
-	}
-
-	public void setCreateDate(Date createDate) {
-		this.createDate = createDate;
-	}
-
-	public Date getUpdateDate() {
-		return updateDate;
-	}
-
-	public void setUpdateDate(Date updateDate) {
-		this.updateDate = updateDate;
-	}
-
-	public boolean isDeductible() {
-		return deductible;
-	}
-
-	public void setDeductible(boolean deductible) {
-		this.deductible = deductible;
-	}
-
-	public boolean isAutoPay() {
-		return autoPay;
-	}
-
-	public void setAutoPay(boolean autoPay) {
-		this.autoPay = autoPay;
-	}
-
-	public String getNotes() {
-		return notes;
-	}
-
-	public void setNotes(String notes) {
-		this.notes = notes;
-	}
+    @Transient
+    private Map<String, Object> fieldValueMap = null;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public List<Gift> getGifts() {
+        return gifts;
+    }
+
+    public void setGifts(List<Gift> gifts) {
+        this.gifts = gifts;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public BigDecimal getAmountPerGift() {
+        return amountPerGift;
+    }
+
+    public void setAmountPerGift(BigDecimal amountPerGift) {
+        this.amountPerGift = amountPerGift;
+    }
+
+    public Integer getNumberOfGifts() {
+        return numberOfGifts;
+    }
+
+    public void setNumberOfGifts(Integer numberOfGifts) {
+        this.numberOfGifts = numberOfGifts;
+    }
+
+    public List<CommitmentCustomField> getCustomFields() {
+        if (commitmentCustomFields == null) {
+            commitmentCustomFields = new ArrayList<CommitmentCustomField>();
+        }
+        return commitmentCustomFields;
+    }
+
+    @SuppressWarnings("unchecked")
+    public Map<String, CustomField> getCustomFieldMap() {
+        if (customFieldMap == null) {
+            customFieldMap = CommitmentCustomFieldMap.buildCustomFieldMap(getCustomFields(), this);
+        }
+        return customFieldMap;
+    }
+
+    @Override
+    public Set<String> getRequiredFields() {
+        return requiredFields;
+    }
+
+    @Override
+    public void setRequiredFields(Set<String> requiredFields) {
+        this.requiredFields = requiredFields;
+    }
+
+    @Override
+    public Map<String, String> getValidationMap() {
+        return validationMap;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    @Override
+    public void setValidationMap(Map<String, String> validationMap) {
+        this.validationMap = validationMap;
+    }
+
+    @Override
+    public Map<String, String> getFieldLabelMap() {
+        return fieldLabelMap;
+    }
+
+    @Override
+    public void setFieldLabelMap(Map<String, String> fieldLabelMap) {
+        this.fieldLabelMap = fieldLabelMap;
+    }
+
+    @Override
+    public Map<String, Object> getFieldValueMap() {
+        return fieldValueMap;
+    }
+
+    @Override
+    public void setFieldValueMap(Map<String, Object> fieldValueMap) {
+        this.fieldValueMap = fieldValueMap;
+    }
+
+    @Override
+    public Site getSite() {
+        return person != null ? person.getSite() : null;
+    }
+
+    public String getCommitmentCode() {
+        return commitmentCode;
+    }
+
+    public void setCommitmentCode(String commitmentCode) {
+        this.commitmentCode = commitmentCode;
+    }
+
+    public String getProjectCode() {
+        return projectCode;
+    }
+
+    public void setProjectCode(String projectCode) {
+        this.projectCode = projectCode;
+    }
+
+    public String getMotivationCode() {
+        return motivationCode;
+    }
+
+    public void setMotivationCode(String motivationCode) {
+        this.motivationCode = motivationCode;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public CommitmentStatusType getStatusType() {
+        return statusType;
+    }
+
+    public void setStatusType(CommitmentStatusType statusType) {
+        this.statusType = statusType;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public boolean isDeductible() {
+        return deductible;
+    }
+
+    public void setDeductible(boolean deductible) {
+        this.deductible = deductible;
+    }
+
+    public boolean isAutoPay() {
+        return autoPay;
+    }
+
+    public void setAutoPay(boolean autoPay) {
+        this.autoPay = autoPay;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
 }

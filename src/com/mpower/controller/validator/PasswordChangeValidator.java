6ec44b49add80a94abe86868eb0c1b7114042d55
jball <jball@0f581344-5646-3a48-8ded-616bfea576c6>
2008-09-30 22:32:56 +0000

    allow password change
    git-svn-id: svn://svn.orangeleap.com/tangerine/mpower/trunk@2295 0f581344-5646-3a48-8ded-616bfea576c6

--- src/com/mpower/controller/validator/PasswordChangeValidator.java
+++ src/com/mpower/controller/validator/PasswordChangeValidator.java
@@ -1,5 +1,6 @@
 package com.mpower.controller.validator;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.validation.Errors;
@@ -22,16 +23,32 @@ public class PasswordChangeValidator implements Validator {
     @Override
     public void validate(Object target, Errors errors) {
         PasswordChange pwChange = (PasswordChange) target;
+        String newPw = StringUtils.trimToEmpty(pwChange.getNewPassword());
+        String newConfirmPw = StringUtils.trimToEmpty(pwChange.getNewPasswordConfirm());
+        String oldPw = StringUtils.trimToEmpty(pwChange.getCurrentPassword());
 
-        String authenticatedPW = SessionServiceImpl.lookupUserPassword();
-        if (!authenticatedPW.equals(pwChange.getCurrentPassword())) {
+        // spoke with Jenn and we're starting:
+        //   old password entered must match current password
+        //   new password and confirm password must match
+        //   new password must be different than current password
+        //   minimum 6 characters
+        //   TODO: force change password every 60 days
+        String authenticatedPw = SessionServiceImpl.lookupUserPassword();
+        if (!authenticatedPw.equals(oldPw)) {
             errors.rejectValue("currentPassword", "currentPasswordIncorrect", null, "current password is incorrect");
-            logger.debug("current password, " + authenticatedPW + ", didn't match " + pwChange.getCurrentPassword());
-        } else if (!pwChange.getNewPassword().equals(pwChange.getNewPasswordConfirm())) {
+            logger.debug("current password, " + authenticatedPw + ", didn't match " + oldPw);
+        } else if (!newPw.equals(newConfirmPw)) {
             errors.rejectValue("newPasswordConfirm", "newPasswordConfirmIncorrect", null, "new passwords must match");
-            logger.debug("new password, " + pwChange.getNewPassword() + ", didn't match confirm password " + pwChange.getNewPasswordConfirm());
+            logger.debug("new password, " + newPw + ", didn't match confirm password " + newConfirmPw);
+        } else if (authenticatedPw.equals(newPw)) {
+            errors.rejectValue("newPassword", "newPasswordMatchesCurrent", null, "new password must be different then current password");
+            logger.debug("new password, " + newPw + ", must be different then current password");
         } else {
             // TODO: validated password requirements here
+            if (newPw.trim().length() < 6) {
+                errors.rejectValue("newPassword", "newPasswordSizeIncorrect", null, "new password must be at least 6 characters");
+                logger.debug("new password, " + newPw + ", must be at least 6 characters");
+            }
         }
     }
 }

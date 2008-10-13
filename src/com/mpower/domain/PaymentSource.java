942ab4ed0aca1983b5a176c6607bf6fa4499d109
jball <jball@0f581344-5646-3a48-8ded-616bfea576c6>
2008-10-13 21:11:55 +0000

    encrypt credit card number and ach account number
    git-svn-id: svn://svn.orangeleap.com/tangerine/mpower/trunk@2464 0f581344-5646-3a48-8ded-616bfea576c6

--- src/com/mpower/domain/PaymentSource.java
+++ src/com/mpower/domain/PaymentSource.java
@@ -20,308 +20,359 @@ import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import com.mpower.util.AES;
+
 @Entity
 @Table(name = "PAYMENT_SOURCE")
 public class PaymentSource implements SiteAware, Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@SuppressWarnings("unused")
-	@Transient
-	private final Log logger = LogFactory.getLog(getClass());
+    @SuppressWarnings("unused")
+    @Transient
+    private final Log logger = LogFactory.getLog(getClass());
 
-	@Id
-	@GeneratedValue
-	@Column(name = "PAYMENT_SOURCE_ID")
-	private Long id;
+    @Id
+    @GeneratedValue
+    @Column(name = "PAYMENT_SOURCE_ID")
+    private Long id;
 
-	@ManyToOne
-	@JoinColumn(name = "PERSON_ID")
-	private Person person;
+    @ManyToOne
+    @JoinColumn(name = "PERSON_ID")
+    private Person person;
 
-	@Column(name = "PAYMENT_NAME")
-	private String name;
+    @Column(name = "PAYMENT_NAME")
+    private String name;
 
-	@Column(name = "PAYMENT_TYPE")
-	private String type;
+    @Column(name = "PAYMENT_TYPE")
+    private String type;
 
-	@Column(name = "CREDIT_CARD_TYPE")
-	private String creditCardType;
+    @Column(name = "CREDIT_CARD_TYPE")
+    private String creditCardType;
 
-	@Column(name = "CREDIT_CARD_NUMBER")
-	private String creditCardNumber;
+    @Column(name = "CREDIT_CARD_NUMBER")
+    private String creditCardNumberEncrypted;
 
-	@Column(name = "CREDIT_CARD_EXPIRATION")
-	private Date creditCardExpiration;
+    @Column(name = "CREDIT_CARD_EXPIRATION")
+    private Date creditCardExpiration;
 
-	@Column(name = "CHECK_NUMBER")
-	private Integer checkNumber;
+    @Column(name = "CHECK_NUMBER")
+    private Integer checkNumber;
 
-	@Column(name = "ACH_TYPE")
-	private String achType;
+    @Column(name = "ACH_TYPE")
+    private String achType;
 
-	@Column(name = "ACH_ROUTING_NUMBER")
-	private String achRoutingNumber;
+    @Column(name = "ACH_ROUTING_NUMBER")
+    private String achRoutingNumber;
 
-	@Column(name = "ACH_ACCOUNT_NUMBER")
-	private String achAccountNumber;
+    @Column(name = "ACH_ACCOUNT_NUMBER")
+    private String achAccountNumberEncrypted;
 
-	@Column(name = "ACTIVE")
-	private boolean active;
+    @Column(name = "ACTIVE")
+    private boolean active;
 
-	@Transient
-	private Integer creditCardExpirationMonth;
+    @Transient
+    private Integer creditCardExpirationMonth;
 
-	@Transient
-	private String creditCardExpirationMonthText;
+    @Transient
+    private String creditCardExpirationMonthText;
 
-	@Transient
-	private Integer creditCardExpirationYear;
+    @Transient
+    private Integer creditCardExpirationYear;
 
-	@Transient
-	private String creditCardNumberDisplay;
+    @Transient
+    private String creditCardNumber;
 
-	// absolutely don't store this in the db - see VISA merchant rules only used
-	// for processing
-	@Transient
-	private String creditCardSecurityCode;
+    // absolutely don't store this in the db - see VISA merchant rules only used for processing
+    @Transient
+    private String creditCardSecurityCode;
 
-	public PaymentSource() {
-	}
+    @Transient
+    private String achAccountNumber;
 
-	public PaymentSource(Person person) {
-		this.person = person;
-	}
+    public PaymentSource() {
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public PaymentSource(Person person) {
+        this.person = person;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public Person getPerson() {
-		return person;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setPerson(Person person) {
-		this.person = person;
-	}
+    public Person getPerson() {
+        return person;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public void setPerson(Person person) {
+        this.person = person;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getType() {
-		return type;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setType(String type) {
-		this.type = type;
-	}
+    public String getType() {
+        return type;
+    }
 
-	public String getCreditCardType() {
-		return creditCardType;
-	}
+    public void setType(String type) {
+        this.type = type;
+    }
 
-	public void setCreditCardType(String creditCardType) {
-		this.creditCardType = creditCardType;
-	}
+    public String getCreditCardType() {
+        return creditCardType;
+    }
 
-	public String getCreditCardNumber() {
-		return creditCardNumber;
-	}
+    public void setCreditCardType(String creditCardType) {
+        this.creditCardType = creditCardType;
+    }
 
-	public void setCreditCardNumber(String creditCardNumber) {
-		this.creditCardNumber = creditCardNumber;
-	}
+    public String getCreditCardNumberEncrypted() {
+        return creditCardNumberEncrypted;
+    }
 
-	public Date getCreditCardExpiration() {
-		return creditCardExpiration;
-	}
+    public void setCreditCardNumberEncrypted(String creditCardNumberEncrypted) {
+        this.creditCardNumberEncrypted = creditCardNumberEncrypted;
+    }
 
-	public void setCreditCardExpiration(Date creditCardExpiration) {
-		this.creditCardExpiration = creditCardExpiration;
-	}
+    public String getCreditCardNumber() {
+        if (creditCardNumberEncrypted != null) {
+            return AES.decrypt(creditCardNumberEncrypted);
+        }
+        return null;
+    }
 
-	public Integer getCheckNumber() {
-		return checkNumber;
-	}
+    public void setCreditCardNumber(String creditCardNumber) {
+        this.creditCardNumber = creditCardNumber;
+        if (creditCardNumber != null) {
+            creditCardNumberEncrypted = AES.encrypt(creditCardNumber);
+        } else {
+            creditCardNumberEncrypted = null;
+        }
+    }
 
-	public void setCheckNumber(Integer checkNumber) {
-		this.checkNumber = checkNumber;
-	}
+    public String getCreditCardNumberDisplay() {
+        String clear = null;
+        if (creditCardNumberEncrypted != null) {
+            clear = AES.decrypt(creditCardNumberEncrypted);
+            if (clear != null && clear.length() >= 4) {
+                return "***-" + clear.substring(clear.length() - 4);
+            }
+        }
+        return clear;
+    }
 
-	public String getAchType() {
-		return achType;
-	}
+    public void setCreditCardNumberDisplay(String str) {
+        // no-op
+    }
 
-	public void setAchType(String achType) {
-		this.achType = achType;
-	}
+    public Date getCreditCardExpiration() {
+        return creditCardExpiration;
+    }
 
-	public String getAchRoutingNumber() {
-		return achRoutingNumber;
-	}
+    public void setCreditCardExpiration(Date creditCardExpiration) {
+        this.creditCardExpiration = creditCardExpiration;
+    }
 
-	public void setAchRoutingNumber(String achRoutingNumber) {
-		this.achRoutingNumber = achRoutingNumber;
-	}
+    public Integer getCheckNumber() {
+        return checkNumber;
+    }
 
-	public String getAchAccountNumber() {
-		return achAccountNumber;
-	}
+    public void setCheckNumber(Integer checkNumber) {
+        this.checkNumber = checkNumber;
+    }
 
-	public void setAchAccountNumber(String achAccountNumber) {
-		this.achAccountNumber = achAccountNumber;
-	}
+    public String getAchType() {
+        return achType;
+    }
 
-	public void setActive(boolean active) {
-		this.active = active;
-	}
+    public void setAchType(String achType) {
+        this.achType = achType;
+    }
 
-	public boolean isActive() {
-		return active;
-	}
+    public String getAchRoutingNumber() {
+        return achRoutingNumber;
+    }
 
-	public Integer getCreditCardExpirationMonth() {
-		if (getCreditCardExpiration() != null) {
-			Calendar calendar = Calendar.getInstance();
-			calendar.setTime(getCreditCardExpiration());
-			creditCardExpirationMonth = calendar.get(Calendar.MONTH) + 1;
-		}
-		return creditCardExpirationMonth;
-	}
+    public void setAchRoutingNumber(String achRoutingNumber) {
+        this.achRoutingNumber = achRoutingNumber;
+    }
 
-	public String getCreditCardExpirationMonthText() {
-		if (getCreditCardExpiration() != null) {
-			Calendar calendar = Calendar.getInstance();
-			calendar.setTime(getCreditCardExpiration());
-			creditCardExpirationMonth = calendar.get(Calendar.MONTH) + 1;
-			String month = String.valueOf(creditCardExpirationMonth);
-			if (month.length() == 1) {
-				month = "0" + month;
-			}
-			creditCardExpirationMonthText = month;
-		}
-		return creditCardExpirationMonthText;
-	}
+    public String getAchAccountNumberEncrypted() {
+        return achAccountNumberEncrypted;
+    }
 
-	public void setCreditCardExpirationMonth(Integer creditCardExpirationMonth) {
-		setExpirationDate(creditCardExpirationMonth, null);
-		this.creditCardExpirationMonth = creditCardExpirationMonth;
-	}
+    public void setAchAccountNumberEncrypted(String achAccountNumberEncrypted) {
+        this.achAccountNumberEncrypted = achAccountNumberEncrypted;
+    }
 
-	public Integer getCreditCardExpirationYear() {
-		if (getCreditCardExpiration() != null) {
-			Calendar calendar = Calendar.getInstance();
-			calendar.setTime(getCreditCardExpiration());
-			creditCardExpirationYear = calendar.get(Calendar.YEAR);
-		}
-		return creditCardExpirationYear;
-	}
+    public String getAchAccountNumber() {
+        if (achAccountNumberEncrypted != null) {
+            return AES.decrypt(achAccountNumberEncrypted);
+        }
+        return null;
+    }
 
-	public void setCreditCardExpirationYear(Integer creditCardExpirationYear) {
-		setExpirationDate(null, creditCardExpirationYear);
-		this.creditCardExpirationYear = creditCardExpirationYear;
-	}
+    public void setAchAccountNumber(String achAccountNumber) {
+        this.achAccountNumber = achAccountNumber;
+        if (achAccountNumber != null) {
+            achAccountNumberEncrypted = AES.encrypt(achAccountNumber);
+        } else {
+            achAccountNumberEncrypted = null;
+        }
+    }
 
-	private void setExpirationDate(Integer month, Integer year) {
-		Calendar calendar = Calendar.getInstance();
-		if (getCreditCardExpiration() != null) {
-			calendar.setTime(getCreditCardExpiration());
-		}
-		if (month != null) {
-			calendar.set(Calendar.MONTH, month - 1);
-		}
-		if (year != null) {
-			calendar.set(Calendar.YEAR, year);
-		}
-		calendar.set(Calendar.DAY_OF_MONTH, 1); // need to reset to 1 prior to
-		// getting max day
-		calendar.set(Calendar.DAY_OF_MONTH, calendar
-				.getActualMaximum(Calendar.DAY_OF_MONTH));
-		calendar.set(Calendar.HOUR, calendar.getActualMaximum(Calendar.HOUR));
-		calendar.set(Calendar.MINUTE, calendar
-				.getActualMaximum(Calendar.MINUTE));
-		calendar.set(Calendar.SECOND, calendar
-				.getActualMaximum(Calendar.SECOND));
-		setCreditCardExpiration(calendar.getTime());
-	}
+    public String getAchAccountNumberDisplay() {
+        String clear = null;
+        if (achAccountNumberEncrypted != null) {
+            clear = AES.decrypt(achAccountNumberEncrypted);
+            if (clear != null && clear.length() >= 4) {
+                return "***-" + clear.substring(clear.length() - 4);
+            }
+        }
+        return clear;
+    }
 
-	public String getCreditCardSecurityCode() {
-		return creditCardSecurityCode;
-	}
+    public void setAchAccountNumberDisplay(String str) {
+        // no-op
+    }
 
-	public void setCreditCardSecurityCode(String creditCardSecurityCode) {
-		this.creditCardSecurityCode = creditCardSecurityCode;
-	}
+    public void setActive(boolean active) {
+        this.active = active;
+    }
 
-	public Site getSite() {
-		return person != null ? person.getSite() : null;
-	}
+    public boolean isActive() {
+        return active;
+    }
 
-	public List<String> getExpirationMonthList() {
-		List<String> monthList = new ArrayList<String>();
-		for (int i = 1; i <= 12; i++) {
-			String month = String.valueOf(i);
-			if (month.length() == 1) {
-				month = "0" + month;
-			}
-			monthList.add(month);
-		}
-		return monthList;
-	}
+    public Integer getCreditCardExpirationMonth() {
+        if (getCreditCardExpiration() != null) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(getCreditCardExpiration());
+            creditCardExpirationMonth = calendar.get(Calendar.MONTH) + 1;
+        }
+        return creditCardExpirationMonth;
+    }
 
-	public List<String> getExpirationYearList() {
-		List<String> yearList = new ArrayList<String>();
-		int year = Calendar.getInstance().get(Calendar.YEAR);
-		for (int i = 0; i < 10; i++) {
-			yearList.add(String.valueOf(year + i));
-		}
-		return yearList;
-	}
+    public String getCreditCardExpirationMonthText() {
+        if (getCreditCardExpiration() != null) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(getCreditCardExpiration());
+            creditCardExpirationMonth = calendar.get(Calendar.MONTH) + 1;
+            String month = String.valueOf(creditCardExpirationMonth);
+            if (month.length() == 1) {
+                month = "0" + month;
+            }
+            creditCardExpirationMonthText = month;
+        }
+        return creditCardExpirationMonthText;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (!(obj instanceof PaymentSource)) {
-			return false;
-		}
-		PaymentSource ps = (PaymentSource) obj;
-		EqualsBuilder eb = new EqualsBuilder();
-		if ("ACH".equals(getType())) {
-			eb.append(achType, ps.achType).append(achAccountNumber,
-					ps.achAccountNumber).append(achRoutingNumber,
-							ps.achRoutingNumber);
-		} else if ("Credit Card".equals(getType())) {
-			eb.append(creditCardType, ps.creditCardType).append(
-					creditCardNumber, ps.creditCardNumber).append(
-							creditCardExpiration, ps.creditCardExpiration).append(
-									creditCardSecurityCode, ps.creditCardSecurityCode);
-		}
-		return eb.isEquals();
-	}
+    public void setCreditCardExpirationMonth(Integer creditCardExpirationMonth) {
+        setExpirationDate(creditCardExpirationMonth, null);
+        this.creditCardExpirationMonth = creditCardExpirationMonth;
+    }
 
-	@Override
-	public int hashCode() {
-		HashCodeBuilder hcb = new HashCodeBuilder();
-		if ("ACH".equals(getType())) {
-			hcb.append(achType).append(achAccountNumber).append(
-					achRoutingNumber);
-		} else if ("Credit Card".equals(getType())) {
-			hcb.append(creditCardType).append(creditCardNumber).append(
-					creditCardExpiration).append(creditCardSecurityCode);
-		}
-		return hcb.hashCode();
-	}
+    public Integer getCreditCardExpirationYear() {
+        if (getCreditCardExpiration() != null) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(getCreditCardExpiration());
+            creditCardExpirationYear = calendar.get(Calendar.YEAR);
+        }
+        return creditCardExpirationYear;
+    }
 
-	public String getCreditCardNumberDisplay() {
-		String creditCardNumberDisplay = "***-" + this.creditCardNumber.substring(this.creditCardNumber.length()-4, this.creditCardNumber.length());
-		return creditCardNumberDisplay;
-	}
+    public void setCreditCardExpirationYear(Integer creditCardExpirationYear) {
+        setExpirationDate(null, creditCardExpirationYear);
+        this.creditCardExpirationYear = creditCardExpirationYear;
+    }
+
+    private void setExpirationDate(Integer month, Integer year) {
+        Calendar calendar = Calendar.getInstance();
+        if (getCreditCardExpiration() != null) {
+            calendar.setTime(getCreditCardExpiration());
+        }
+        if (month != null) {
+            calendar.set(Calendar.MONTH, month - 1);
+        }
+        if (year != null) {
+            calendar.set(Calendar.YEAR, year);
+        }
+        calendar.set(Calendar.DAY_OF_MONTH, 1); // need to reset to 1 prior to
+        // getting max day
+        calendar.set(Calendar.DAY_OF_MONTH, calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
+        calendar.set(Calendar.HOUR, calendar.getActualMaximum(Calendar.HOUR));
+        calendar.set(Calendar.MINUTE, calendar.getActualMaximum(Calendar.MINUTE));
+        calendar.set(Calendar.SECOND, calendar.getActualMaximum(Calendar.SECOND));
+        setCreditCardExpiration(calendar.getTime());
+    }
+
+    public String getCreditCardSecurityCode() {
+        return creditCardSecurityCode;
+    }
+
+    public void setCreditCardSecurityCode(String creditCardSecurityCode) {
+        this.creditCardSecurityCode = creditCardSecurityCode;
+    }
+
+    public Site getSite() {
+        return person != null ? person.getSite() : null;
+    }
+
+    public List<String> getExpirationMonthList() {
+        List<String> monthList = new ArrayList<String>();
+        for (int i = 1; i <= 12; i++) {
+            String month = String.valueOf(i);
+            if (month.length() == 1) {
+                month = "0" + month;
+            }
+            monthList.add(month);
+        }
+        return monthList;
+    }
+
+    public List<String> getExpirationYearList() {
+        List<String> yearList = new ArrayList<String>();
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        for (int i = 0; i < 10; i++) {
+            yearList.add(String.valueOf(year + i));
+        }
+        return yearList;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof PaymentSource)) {
+            return false;
+        }
+        PaymentSource ps = (PaymentSource) obj;
+        EqualsBuilder eb = new EqualsBuilder();
+        if ("ACH".equals(getType())) {
+            eb.append(achType, ps.achType).append(achAccountNumber, ps.achAccountNumber).append(achRoutingNumber, ps.achRoutingNumber);
+        } else if ("Credit Card".equals(getType())) {
+            eb.append(creditCardType, ps.creditCardType).append(creditCardNumber, ps.creditCardNumber).append(creditCardExpiration, ps.creditCardExpiration).append(creditCardSecurityCode, ps.creditCardSecurityCode);
+        }
+        return eb.isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        HashCodeBuilder hcb = new HashCodeBuilder();
+        if ("ACH".equals(getType())) {
+            hcb.append(achType).append(achAccountNumber).append(achRoutingNumber);
+        } else if ("Credit Card".equals(getType())) {
+            hcb.append(creditCardType).append(creditCardNumber).append(creditCardExpiration).append(creditCardSecurityCode);
+        }
+        return hcb.hashCode();
+    }
 }

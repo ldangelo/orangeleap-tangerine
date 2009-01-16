3dc59108a0e145b1f88072671528608635ede804
alo <alo@0f581344-5646-3a48-8ded-616bfea576c6>
2009-01-16 20:44:01 +0000

    added FieldHtmlName
    git-svn-id: svn://svn.orangeleap.com/tangerine/mpower/trunk@3691 0f581344-5646-3a48-8ded-616bfea576c6

--- src/com/mpower/web/customization/FieldVO.java
+++ src/com/mpower/web/customization/FieldVO.java
@@ -65,6 +65,10 @@ public class FieldVO {
         return fieldName;
     }
 
+    public String getFieldHtmlName() {
+        return fieldName == null ? "" : fieldName.replace('.', '_').replace('[', '-').replace(']', '-');
+    }
+    
     public FieldType getFieldType() {
         return fieldType;
     }

eac0c29ee280e84a9330f1f4bdccf1741ec47d46
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-01-19 21:04:56 +0000

    remove default code type
    git-svn-id: svn://svn.orangeleap.com/tangerine/mpower/trunk@3720 0f581344-5646-3a48-8ded-616bfea576c6

---
+++ db/dev/devSetup/codeType.sql
@@ -0,0 +1,4 @@
+INSERT INTO CODE_TYPE (NAME, LABEL, SITE_NAME) VALUES ('projectCode','Project Code',"company1");
+INSERT INTO CODE_TYPE (NAME, LABEL, SITE_NAME) VALUES ('motivationCode','Motivation Code',"company1");
+INSERT INTO CODE_TYPE (NAME, LABEL, SITE_NAME) VALUES ('commitmentCode','Commitment Code',"company1");
+

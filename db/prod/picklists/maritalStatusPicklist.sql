44af136d98b4111c80fe2a81efb57000604c4495
alo <alo@0f581344-5646-3a48-8ded-616bfea576c6>
2009-01-28 05:53:56 +0000

    TANGERINE-198: changed references to escaped IDs
    git-svn-id: svn://svn.orangeleap.com/tangerine/mpower/trunk@3922 0f581344-5646-3a48-8ded-616bfea576c6

--- db/prod/picklists/maritalStatusPicklist.sql
+++ db/prod/picklists/maritalStatusPicklist.sql
@@ -1,9 +1,10 @@
 -- Marital Status Picklist
+-- NOTE: reference values characters are escaped for HTML IDs -> '.' is replaced with '_' and ']' and '[' are replaced with '-'; see FieldVO#escapeChars()
 
 INSERT INTO PICKLIST (PICKLIST_ID, PICKLIST_NAME) VALUES ('maritalStatus', 'maritalStatus');
-INSERT INTO PICKLIST_ITEM (PICKLIST_ID, ITEM_NAME, DEFAULT_DISPLAY_VALUE, REFERENCE_VALUE, ITEM_ORDER) VALUES ('maritalStatus', 'Unknown', 'Unknown', '', 1);
+INSERT INTO PICKLIST_ITEM (PICKLIST_ID, ITEM_NAME, DEFAULT_DISPLAY_VALUE, REFERENCE_VALUE, ITEM_ORDER) VALUES ('maritalStatus', 'Unknown', 'Unknown', 'li:has(#customFieldMap-individual_spouse-_value)', 1);
 INSERT INTO PICKLIST_ITEM (PICKLIST_ID, ITEM_NAME, DEFAULT_DISPLAY_VALUE, REFERENCE_VALUE, ITEM_ORDER) VALUES ('maritalStatus', 'Single', 'Single', '', 2);
-INSERT INTO PICKLIST_ITEM (PICKLIST_ID, ITEM_NAME, DEFAULT_DISPLAY_VALUE, REFERENCE_VALUE, ITEM_ORDER) VALUES ('maritalStatus', 'Married', 'Married', 'li:has(#customFieldMap\\[individual\\.spouse\\]\\.value)', 3);
-INSERT INTO PICKLIST_ITEM (PICKLIST_ID, ITEM_NAME, DEFAULT_DISPLAY_VALUE, REFERENCE_VALUE, ITEM_ORDER) VALUES ('maritalStatus', 'Divorced', 'Divorced', '', 4);
-INSERT INTO PICKLIST_ITEM (PICKLIST_ID, ITEM_NAME, DEFAULT_DISPLAY_VALUE, REFERENCE_VALUE, ITEM_ORDER) VALUES ('maritalStatus', 'Widowed', 'Widowed', '', 5);
-INSERT INTO PICKLIST_ITEM (PICKLIST_ID, ITEM_NAME, DEFAULT_DISPLAY_VALUE, REFERENCE_VALUE, ITEM_ORDER) VALUES ('maritalStatus', 'Engaged', 'Engaged', '', 6);
+INSERT INTO PICKLIST_ITEM (PICKLIST_ID, ITEM_NAME, DEFAULT_DISPLAY_VALUE, REFERENCE_VALUE, ITEM_ORDER) VALUES ('maritalStatus', 'Married', 'Married', 'li:has(#customFieldMap-individual_spouse-_value)', 3);
+INSERT INTO PICKLIST_ITEM (PICKLIST_ID, ITEM_NAME, DEFAULT_DISPLAY_VALUE, REFERENCE_VALUE, ITEM_ORDER) VALUES ('maritalStatus', 'Divorced', 'Divorced', 'li:has(#customFieldMap-individual_spouse-_value)', 4);
+INSERT INTO PICKLIST_ITEM (PICKLIST_ID, ITEM_NAME, DEFAULT_DISPLAY_VALUE, REFERENCE_VALUE, ITEM_ORDER) VALUES ('maritalStatus', 'Widowed', 'Widowed', 'li:has(#customFieldMap-individual_spouse-_value)', 5);
+INSERT INTO PICKLIST_ITEM (PICKLIST_ID, ITEM_NAME, DEFAULT_DISPLAY_VALUE, REFERENCE_VALUE, ITEM_ORDER) VALUES ('maritalStatus', 'Engaged', 'Engaged', 'li:has(#customFieldMap-individual_spouse-_value)', 6);

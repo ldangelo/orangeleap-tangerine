6a44a282c7e0680396dad9f9cc594f9ef3ed3ed3
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-02-05 22:37:12 +0000

    make read only
    git-svn-id: svn://svn.orangeleap.com/tangerine/mpower/trunk@4086 0f581344-5646-3a48-8ded-616bfea576c6

--- db/prod/domain/communicationHistory.sql
+++ db/prod/domain/communicationHistory.sql
@@ -6,5 +6,6 @@ INSERT INTO FIELD_DEFINITION (FIELD_DEFINITION_ID, ENTITY_TYPE, FIELD_NAME, DEFA
 INSERT INTO FIELD_DEFINITION (FIELD_DEFINITION_ID, ENTITY_TYPE, FIELD_NAME, DEFAULT_LABEL, FIELD_TYPE) VALUES ('communicationHistory.id.ReadOnly', 'communicationHistory', 'id', 'Reference Number', 'READ_ONLY_TEXT');
 INSERT INTO FIELD_DEFINITION (FIELD_DEFINITION_ID, ENTITY_TYPE, FIELD_NAME, DEFAULT_LABEL, FIELD_TYPE) VALUES ('communicationHistory.transactionDate.ReadOnly', 'communicationHistory', 'transactionDate', 'Date', 'DATE_DISPLAY');
 INSERT INTO FIELD_DEFINITION (FIELD_DEFINITION_ID, ENTITY_TYPE, FIELD_NAME, DEFAULT_LABEL, FIELD_TYPE) VALUES ('communicationHistory.comments.ReadOnly', 'communicationHistory', 'comments', 'Comments', 'READ_ONLY_TEXT');
+INSERT INTO FIELD_DEFINITION (FIELD_DEFINITION_ID, ENTITY_TYPE, FIELD_NAME, DEFAULT_LABEL, FIELD_TYPE) VALUES ('communicationHistory.customFieldMap[followUpCode].ReadOnly', 'communicationHistory', 'customFieldMap[followUpCode]', 'Follow Up Code', 'READ_ONLY_TEXT');
 
 

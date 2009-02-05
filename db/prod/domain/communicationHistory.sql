e5c0296afbd2e26c8598f1f39dd5518371e7a1e4
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-02-05 19:50:37 +0000

    TANGERINE-50 initial screen fields
    git-svn-id: svn://svn.orangeleap.com/tangerine/mpower/trunk@4076 0f581344-5646-3a48-8ded-616bfea576c6

---
+++ db/prod/domain/communicationHistory.sql
@@ -0,0 +1,6 @@
+INSERT INTO FIELD_DEFINITION (FIELD_DEFINITION_ID, ENTITY_TYPE, FIELD_NAME, DEFAULT_LABEL, FIELD_TYPE) VALUES ('communicationHistory.id', 'communicationHistory', 'id', 'Reference Number', 'TEXT');
+INSERT INTO FIELD_DEFINITION (FIELD_DEFINITION_ID, ENTITY_TYPE, FIELD_NAME, DEFAULT_LABEL, FIELD_TYPE) VALUES ('communicationHistory.transactionDate', 'communicationHistory', 'transactionDate', 'Date', 'DATE');
+INSERT INTO FIELD_DEFINITION (FIELD_DEFINITION_ID, ENTITY_TYPE, FIELD_NAME, DEFAULT_LABEL, FIELD_TYPE) VALUES ('communicationHistory.comments', 'communicationHistory', 'comments', 'Comments', 'TEXT');
+INSERT INTO FIELD_DEFINITION (FIELD_DEFINITION_ID, ENTITY_TYPE, FIELD_NAME, DEFAULT_LABEL, FIELD_TYPE) VALUES ('communicationHistory.customFieldMap[followUpCode]', 'communicationHistory', 'customFieldMap[followUpCode]', 'Follow Up Code', 'TEXT');
+
+

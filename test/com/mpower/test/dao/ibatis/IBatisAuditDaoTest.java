4b6b6b99bae2f38387982b22fc7bcf375f23a705
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-02-24 19:55:43 +0000

    ibatis Audit tests
    git-svn-id: svn://svn.orangeleap.com/tangerine/mpower/trunk@4425 0f581344-5646-3a48-8ded-616bfea576c6

---
+++ test/com/mpower/test/dao/ibatis/IBatisAuditDaoTest.java
@@ -0,0 +1,50 @@
+package com.mpower.test.dao.ibatis;
+
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.mpower.dao.AuditDao;
+import com.mpower.domain.model.Audit;
+import com.mpower.domain.model.customization.Code;
+import com.mpower.domain.model.customization.CodeType;
+import com.mpower.type.AuditType;
+
+public class IBatisAuditDaoTest extends AbstractIBatisTest {
+    
+    /** Logger for this class and subclasses */
+    protected final Log logger = LogFactory.getLog(getClass());
+    
+    private AuditDao auditDao;
+
+    @BeforeMethod
+    public void setupMocks() {
+    	auditDao = (AuditDao)super.applicationContext.getBean("auditDAO");
+    }
+    
+    private static final String TEST_USER = "testuser";
+    
+    @Test(groups = { "createAuditEntry" })
+    public void testListCodeTypes() throws Exception {
+    	Audit audit = new Audit();
+    	audit.setAuditType(AuditType.CREATE);
+    	audit.setObjectId(new Long(1));
+    	audit.setDate(new java.util.Date());
+    	audit.setEntityType("gift");
+    	audit.setUser(TEST_USER);
+    	audit.setDescription("test audit");
+    	audit = auditDao.auditObject(audit);
+    	assert audit.getSiteName().equals("company1");
+    } 
+
+    @Test(groups = { "testReadCodeType" }, dependsOnGroups = { "createAuditEntry" })
+    public void testReadCodeType() throws Exception {
+    	List<Audit> list = auditDao.allAuditHistoryForSite();
+        assert list != null && list.size() > 0 && list.get(0).getUser().equals(TEST_USER);
+    } 
+    
+    
+}

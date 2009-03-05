9126aba3fa07c74d5a0070234963e6718ad111c9
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-03-05 19:20:27 +0000

    code type defaults
    git-svn-id: svn://svn.orangeleap.com/tangerine/mpower/trunk@4651 0f581344-5646-3a48-8ded-616bfea576c6

--- db/dev/devSetup/codeType.sql
+++ db/dev/devSetup/codeType.sql
@@ -1,4 +1,4 @@
-INSERT INTO CODE_TYPE (ID, NAME, LABEL, SITE_NAME) VALUES (1, 'projectCode','Designation Code', 'company1');
-INSERT INTO CODE_TYPE (ID, NAME, LABEL, SITE_NAME) VALUES (2, 'motivationCode','Motivation Code', 'company1');
-INSERT INTO CODE_TYPE (ID, NAME, LABEL, SITE_NAME) VALUES (4, 'currencyCode', 'Currency Code', 'company1');
+INSERT INTO CODE_TYPE (ID, NAME, LABEL, SITE_NAME) VALUES (11, 'projectCode','Designation Code', 'company1');
+INSERT INTO CODE_TYPE (ID, NAME, LABEL, SITE_NAME) VALUES (22, 'motivationCode','Motivation Code', 'company1');
+INSERT INTO CODE_TYPE (ID, NAME, LABEL, SITE_NAME) VALUES (33, 'currencyCode', 'Currency Code', 'company1');
 
---
+++ db/prod/picklists/codeType.sql
@@ -0,0 +1,4 @@
+INSERT INTO CODE_TYPE (ID, NAME, LABEL, SITE_NAME) VALUES (1, 'projectCode', 'Designation Code', null);
+INSERT INTO CODE_TYPE (ID, NAME, LABEL, SITE_NAME) VALUES (2, 'motivationCode', 'Motivation Code', null);
+INSERT INTO CODE_TYPE (ID, NAME, LABEL, SITE_NAME) VALUES (10, 'currencyCode', 'Currency Code', null);
+

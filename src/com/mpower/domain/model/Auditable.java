1f618564017faa3dcc0b88253edc40870d9d2afa
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-03-06 18:41:27 +0000

    add Auditable for iBatis model classes
    git-svn-id: svn://svn.orangeleap.com/tangerine/mpower/trunk@4681 0f581344-5646-3a48-8ded-616bfea576c6

---
+++ src/com/mpower/domain/model/Auditable.java
@@ -0,0 +1,17 @@
+package com.mpower.domain.model;
+
+
+/**
+ * Use this class to indicate an entity that is auditable, but not Viewable (see interface)
+ * @author jball
+ */
+public interface Auditable {
+
+    public Auditable getOriginalObject();
+
+    public void setOriginalObject(Auditable auditable);
+
+    public Long getId();
+
+    public Person getPerson();
+}

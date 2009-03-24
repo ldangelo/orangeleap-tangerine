c41f742857cca5e043103c02943ac677c6b28df8
alo <alo@0f581344-5646-3a48-8ded-616bfea576c6>
2009-03-24 17:24:45 +0000

    check for existence of company1, 2, 3 schemas before dropping
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@5114 0f581344-5646-3a48-8ded-616bfea576c6

--- db/clearSchema.sql
+++ db/clearSchema.sql
@@ -1,11 +1,11 @@
 drop schema orangeleap;
 create schema orangeleap;
 
-drop schema company1;
+DROP schema IF EXISTS `company1`;
 create schema company1;
-drop schema company2;
+DROP schema IF EXISTS `company2`;
 create schema company2;
-drop schema company3;
+DROP schema IF EXISTS `company3`;
 create schema company3;
 
 use orangeleap;

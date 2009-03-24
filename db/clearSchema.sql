7d864365d96c5e163e66fc29dfc2e930b584ba38
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-03-24 16:51:31 +0000

    add other schemas
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@5112 0f581344-5646-3a48-8ded-616bfea576c6

--- db/clearSchema.sql
+++ db/clearSchema.sql
@@ -1,3 +1,11 @@
 drop schema orangeleap;
 create schema orangeleap;
+
+drop schema company1;
+create schema company1;
+drop schema company2;
+create schema company2;
+drop schema company3;
+create schema company3;
+
 use orangeleap;

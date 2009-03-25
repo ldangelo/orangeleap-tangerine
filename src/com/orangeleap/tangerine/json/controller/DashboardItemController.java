2e34019879f0b5a33572f53914bb754938ff19ec
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-03-25 16:22:22 +0000

    TANGERINE-173
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@5139 0f581344-5646-3a48-8ded-616bfea576c6

---
+++ src/com/orangeleap/tangerine/json/controller/DashboardItemController.java
@@ -0,0 +1,48 @@
+package com.orangeleap.tangerine.json.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.orangeleap.tangerine.domain.customization.DashboardData;
+import com.orangeleap.tangerine.domain.customization.DashboardItem;
+import com.orangeleap.tangerine.service.customization.DashboardService;
+
+/**
+ * This controller handles JSON requests for populating the dashboard
+ * @version 1.0
+ */
+@Controller
+public class DashboardItemController {
+
+    /** Logger for this class and subclasses */
+    protected final Log logger = LogFactory.getLog(getClass());
+
+    @Resource(name="dashboardService")
+    private DashboardService dashboardService;
+
+    @SuppressWarnings("unchecked")
+    @RequestMapping("/dashboardItemsData.json")
+    public ModelMap getDashboardItemsData(HttpServletRequest request) {
+    	
+    	List<DashboardItem> items = dashboardService.getDashboard();
+    	List<DashboardData> itemsData = new ArrayList<DashboardData>();
+    	for (DashboardItem item : items) {
+    		DashboardData data = dashboardService.getDashboardQueryContent(item);
+    		itemsData.add(data);
+    	}
+    	
+        ModelMap map = new ModelMap();
+        map.put("itemsData", itemsData);
+        return map;
+    }
+
+}

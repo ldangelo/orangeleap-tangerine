4657dd81390cf991378dad98aec8b79a2fef2474
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-03-26 20:07:46 +0000

    make highest default value 999 leaving space below for new default fields to be added later
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@5164 0f581344-5646-3a48-8ded-616bfea576c6

--- db/prod/picklists/tributePicklist.sql
+++ db/prod/picklists/tributePicklist.sql
@@ -1,4 +1,5 @@
-INSERT INTO PICKLIST (PICKLIST_ID, PICKLIST_NAME_ID, PICKLIST_NAME, PICKLIST_DESC) VALUES (43, 'customFieldMap[tribute]', 'customFieldMap[tribute]', 'Tribute');
-INSERT INTO PICKLIST_ITEM (PICKLIST_ID, ITEM_NAME, DEFAULT_DISPLAY_VALUE, ITEM_ORDER) VALUES (43, 'inHonorOf', 'In Honor Of', 1);
-INSERT INTO PICKLIST_ITEM (PICKLIST_ID, ITEM_NAME, DEFAULT_DISPLAY_VALUE, ITEM_ORDER) VALUES (43, 'inMemoryOf', 'In Memory Of', 2);
-INSERT INTO PICKLIST_ITEM (PICKLIST_ID, ITEM_NAME, DEFAULT_DISPLAY_VALUE, ITEM_ORDER) VALUES (43, 'inPerpetualMemoryOf', 'In Perpetual Memory Of', 3);
+-- We need to leave space under 999 for adding default fields later.  New site-specific fields will now start being automatically added by the system starting at 1000.
+INSERT INTO PICKLIST (PICKLIST_ID, PICKLIST_NAME_ID, PICKLIST_NAME, PICKLIST_DESC) VALUES (999, 'customFieldMap[tribute]', 'customFieldMap[tribute]', 'Tribute');
+INSERT INTO PICKLIST_ITEM (PICKLIST_ID, ITEM_NAME, DEFAULT_DISPLAY_VALUE, ITEM_ORDER) VALUES (999, 'inHonorOf', 'In Honor Of', 1);
+INSERT INTO PICKLIST_ITEM (PICKLIST_ID, ITEM_NAME, DEFAULT_DISPLAY_VALUE, ITEM_ORDER) VALUES (999, 'inMemoryOf', 'In Memory Of', 2);
+INSERT INTO PICKLIST_ITEM (PICKLIST_ID, ITEM_NAME, DEFAULT_DISPLAY_VALUE, ITEM_ORDER) VALUES (999, 'inPerpetualMemoryOf', 'In Perpetual Memory Of', 3);

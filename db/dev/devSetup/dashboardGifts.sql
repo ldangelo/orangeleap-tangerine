acd8163f346338508f47722e1036712824d12c5f
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-03-30 20:04:29 +0000

    TANGERINE-482
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@5209 0f581344-5646-3a48-8ded-616bfea576c6

--- db/dev/devSetup/dashboardGifts.sql
+++ db/dev/devSetup/dashboardGifts.sql
@@ -1,7 +1,12 @@
 update GIFT set TRANSACTION_DATE = adddate(now(), GIFT_ID-200) where GIFT_ID < 200;
+update GIFT set DONATION_DATE = adddate(now(), GIFT_ID-200) where GIFT_ID < 200;
 update GIFT set AMOUNT = GIFT_ID where GIFT_ID < 200;
 update DISTRO_LINE set MOTIVATION_CODE = "020303" where GIFT_ID > 197 and GIFT_ID <= 199;
 update DISTRO_LINE set MOTIVATION_CODE = "020302" where GIFT_ID > 196 and GIFT_ID <= 197;
 update DISTRO_LINE set MOTIVATION_CODE = "020301" where GIFT_ID > 195 and GIFT_ID <= 196;
 update DISTRO_LINE set MOTIVATION_CODE = "0201" where GIFT_ID > 192 and GIFT_ID <= 195;
 
+update DISTRO_LINE set PROJECT_CODE = "001000" where GIFT_ID > 197 and GIFT_ID <= 199;
+update DISTRO_LINE set PROJECT_CODE = "001100" where GIFT_ID > 194 and GIFT_ID <= 197;
+update DISTRO_LINE set PROJECT_CODE = "001111" where GIFT_ID > 193 and GIFT_ID <= 194;
+update DISTRO_LINE set PROJECT_CODE = "001500" where GIFT_ID > 192 and GIFT_ID <= 193;

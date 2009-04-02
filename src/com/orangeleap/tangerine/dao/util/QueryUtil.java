b0455ddaa90fc29213c1619e8cc9af63c93ec1ff
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-04-02 20:21:03 +0000

    remove unused code
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@5257 0f581344-5646-3a48-8ded-616bfea576c6

--- src/com/orangeleap/tangerine/dao/util/QueryUtil.java
+++ src/com/orangeleap/tangerine/dao/util/QueryUtil.java
@@ -128,26 +128,5 @@ public class QueryUtil {
     
     
     
-    public static StringBuilder getCustomString(Map<String, String> customParams, LinkedHashMap<String, Object> parameterMap) {
-        StringBuilder customString = new StringBuilder();
-        if (customParams != null && !customParams.isEmpty()) {
-            int paramCount = 1;
-            for (Map.Entry<String, String> pair : customParams.entrySet()) {
-                String key = pair.getKey();
-                Object value = pair.getValue();
-                customString.append(" AND EXISTS ( SELECT giftCustomField.id FROM com.orangeleap.tangerine.domain.GiftCustomField giftCustomField WHERE giftCustomField.gift.id = gift.id AND (");
-                customString.append("giftCustomField.customField.name = :name");
-                customString.append(paramCount);
-                customString.append(" AND giftCustomField.customField.value LIKE :value");
-                customString.append(paramCount);
-                parameterMap.put("name" + paramCount, key);
-                parameterMap.put("value" + paramCount, "%" + value + "%");
-                customString.append("))");
-                paramCount++;
-            }
-        }
-        return customString;
-    }
-     
  
 }

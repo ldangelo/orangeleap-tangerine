afeebe04ba602b47b90d309f7ee118f98975b351
alo <alo@0f581344-5646-3a48-8ded-616bfea576c6>
2009-04-03 19:52:48 +0000

    fix for build and testing of rules
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@5282 0f581344-5646-3a48-8ded-616bfea576c6

--- src/com/orangeleap/tangerine/service/rule/DroolsRuleAgent.java
+++ src/com/orangeleap/tangerine/service/rule/DroolsRuleAgent.java
@@ -8,7 +8,6 @@ import org.drools.agent.RuleAgent;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.util.StringUtils;
 
 public class DroolsRuleAgent implements ApplicationContextAware {
 	private static final Log logger = LogFactory.getLog(DroolsRuleAgent.class);
@@ -16,27 +15,15 @@ public class DroolsRuleAgent implements ApplicationContextAware {
 	private ApplicationContext applicationContext;
 	private final Properties droolsProperties;
 
-	public DroolsRuleAgent(String pollInterval, String cacheDir, String packageDir) {
+	public DroolsRuleAgent(final String pollInterval, final String cacheDir, final String packageDir) {
 	    if (logger.isDebugEnabled()) {
 	        logger.debug("DroolsRuleAgent: pollInterval = " + pollInterval + " cacheDir = " + cacheDir + " packageDir = " + packageDir);
 	    }
 		droolsProperties = new Properties();
         droolsProperties.put("newInstance", "false");
         droolsProperties.put("name", "orangeleap");
-        String interval = System.getProperty("overrideDroolsInterval");
-        if (StringUtils.hasText(interval)) {
-            pollInterval = interval;
-        }
         droolsProperties.put("poll", pollInterval);
-        String cache = System.getProperty("overrideDroolsCache");
-        if (StringUtils.hasText(cache)) {
-            cacheDir = cache;
-        }
         droolsProperties.put("cache", cacheDir);
-        String thisPackage = System.getProperty("overrideDroolsPackage");
-        if (StringUtils.hasText(thisPackage)) {
-            packageDir = thisPackage;
-        }
         droolsProperties.put("dir", packageDir);
 	}
 	

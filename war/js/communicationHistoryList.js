ddb780b7eb2c1a0a5b420a7af8c5f092aaaa0991
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-04-16 20:35:11 +0000

    misspelling of width
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@5473 0f581344-5646-3a48-8ded-616bfea576c6

--- war/js/communicationHistoryList.js
+++ war/js/communicationHistoryList.js
@@ -44,7 +44,7 @@ Ext.onReady(function() {
         store: CommunicationHistory.store,
         columns: [
             {header: '', width: 65, dataIndex: 'id', sortable: true, renderer: CommunicationHistory.entityViewRenderer},
-            {header: 'Date', widht: 100, dataIndex: 'date', sortable: true, renderer: Ext.util.Format.dateRenderer('m-d-y')},
+            {header: 'Date', width: 100, dataIndex: 'date', sortable: true, renderer: Ext.util.Format.dateRenderer('m-d-y')},
             {header: 'Type', width: 65, dataIndex: 'type', sortable: true},
             {header: 'Comments', width: 200,  dataIndex: 'comments', sortable: true, renderer: CommunicationHistory.descriptionRenderer}
         ],

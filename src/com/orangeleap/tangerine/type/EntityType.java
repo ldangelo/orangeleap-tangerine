3e4a22fc463834e9d60174c5ea3d76f68e67f76f
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-04-22 14:43:13 +0000

    add constituentCustomFieldRelationship
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@5562 0f581344-5646-3a48-8ded-616bfea576c6

--- src/com/orangeleap/tangerine/type/EntityType.java
+++ src/com/orangeleap/tangerine/type/EntityType.java
@@ -20,5 +20,6 @@ public enum EntityType {
     selectedAddress,
     selectedEmail,
     selectedPhone,
-    selectedPaymentSource
+    selectedPaymentSource,
+    constituentCustomFieldRelationship
 }

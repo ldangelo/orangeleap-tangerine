79ba3fb8e5e61734e68c2fb3cc8e882a3c13dae8
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-04-30 18:32:05 +0000

    TANGERINE-567 remove ability to edit date ranges for roles for now
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@5729 0f581344-5646-3a48-8ded-616bfea576c6

--- src/com/orangeleap/tangerine/controller/relationship/RelationshipListController.java
+++ src/com/orangeleap/tangerine/controller/relationship/RelationshipListController.java
@@ -74,13 +74,15 @@ public class RelationshipListController extends SimpleFormController {
 			}
 		}
 		
-		// Need to be able to edit date ranges on these two non-relationship custom fields since they may affect the visibility of other relationship custom fields.
+// Support for editing date ranges on these two non-relationship custom fields since they may affect the visibility of other relationship custom fields.
+/*
 		FieldDefinition fd1 = fieldDao.readFieldDefinition("person.customFieldMap[constituentIndividualRoles]");
 		fd1.setDefaultLabel(fd1.getDefaultLabel() + " (Individual)");
 		fds.add(fd1);
 		FieldDefinition fd2 = fieldDao.readFieldDefinition("person.customFieldMap[constituentOrganizationRoles]");
 		fd2.setDefaultLabel(fd2.getDefaultLabel() + " (Organization)");
 		fds.add(fd2);
+*/
 		
 		Collections.sort(fds, new Comparator<FieldDefinition>() {
 			@Override

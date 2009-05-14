ccde8d282e6b2d8c75d6341b0e887641d2942067
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-05-14 14:16:36 +0000

    clear demo and sandbox also
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@5898 0f581344-5646-3a48-8ded-616bfea576c6

--- db/clearSchema.sql
+++ db/clearSchema.sql
@@ -7,5 +7,9 @@ DROP schema IF EXISTS `company2`;
 create schema company2;
 DROP schema IF EXISTS `company3`;
 create schema company3;
+DROP schema IF EXISTS `demo`;
+create schema demo;
+DROP schema IF EXISTS `sandbox`;
+create schema sandbox;
 
 

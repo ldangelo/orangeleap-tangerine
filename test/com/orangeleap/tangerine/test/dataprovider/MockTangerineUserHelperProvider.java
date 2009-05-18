181ef6957bca29544cb75236eff58af1b56cb4f3
alo <alo@0f581344-5646-3a48-8ded-616bfea576c6>
2009-05-18 20:18:09 +0000

    fix for build - more methods for Mock TangerineUserHelper
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@5944 0f581344-5646-3a48-8ded-616bfea576c6

--- test/com/orangeleap/tangerine/test/dataprovider/MockTangerineUserHelperProvider.java
+++ test/com/orangeleap/tangerine/test/dataprovider/MockTangerineUserHelperProvider.java
@@ -1,10 +1,14 @@
 package com.orangeleap.tangerine.test.dataprovider;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
 
+import com.orangeleap.tangerine.security.TangerineAuthenticationToken;
 import com.orangeleap.tangerine.util.TangerineUserHelper;
 
 public class MockTangerineUserHelperProvider {
@@ -15,12 +19,16 @@ public class MockTangerineUserHelperProvider {
     public static TangerineUserHelper createMockUserHelper() {
         final Mockery tangerineUserHelperMock = new Mockery();
         final TangerineUserHelper tangerineUserHelper = tangerineUserHelperMock.mock(TangerineUserHelper.class);
-
+        final List<String> roles = new ArrayList<String>();
+        roles.add("ROLE_USER");
+        
         tangerineUserHelperMock.checking(new Expectations() {{
             allowing (tangerineUserHelper).lookupUserId(); will(returnValue(new Long(1)));
             allowing (tangerineUserHelper).lookupUserName(); will(returnValue("nryan"));
             allowing (tangerineUserHelper).lookupUserSiteName(); will(returnValue("company1"));
             allowing (tangerineUserHelper).lookupUserPassword(); will(returnValue("password"));
+            allowing (tangerineUserHelper).lookupUserRoles(); will(returnValue(roles));
+            allowing (tangerineUserHelper).getToken(); will(returnValue(new TangerineAuthenticationToken("", "", "company1")));
         }});
         return tangerineUserHelper;
     }

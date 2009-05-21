2ad14a8abfea37ee3d535d788261eb9cb81ed520
alo <alo@0f581344-5646-3a48-8ded-616bfea576c6>
2009-05-21 17:54:43 +0000

    do not allow previously entered gifts to be retrieved with GiftFormController
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@6005 0f581344-5646-3a48-8ded-616bfea576c6

--- src/com/orangeleap/tangerine/controller/gift/GiftFormController.java
+++ src/com/orangeleap/tangerine/controller/gift/GiftFormController.java
@@ -41,9 +41,7 @@ public class GiftFormController extends TangerineConstituentAttributesFormContro
 
     @Override
     protected AbstractEntity findEntity(HttpServletRequest request) {
-        // TODO: if the user navigates directly to gift.htm with no personId, we should redirect to giftSearch.htm
-        return giftService.readGiftByIdCreateIfNull(super.getConstituent(request), request.getParameter(StringConstants.GIFT_ID), 
-                request.getParameter(StringConstants.RECURRING_GIFT_ID));
+        return giftService.createDefaultGift(super.getConstituent(request));
     }
 
     @SuppressWarnings("unchecked")

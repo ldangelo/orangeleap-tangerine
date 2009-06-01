ec789931027e61737860b8b7d4f340b78e9c835c
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-06-01 19:59:48 +0000

    use spring BindException
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@6170 0f581344-5646-3a48-8ded-616bfea576c6

--- src/com/orangeleap/tangerine/service/communication/EmailService.java
+++ src/com/orangeleap/tangerine/service/communication/EmailService.java
@@ -1,7 +1,5 @@
 package com.orangeleap.tangerine.service.communication;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
@@ -13,7 +11,6 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.net.BindException;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
@@ -30,6 +27,7 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.validation.BindException;
 
 import com.jaspersoft.jasperserver.api.metadata.xml.domain.impl.ResourceDescriptor;
 import com.jaspersoft.jasperserver.api.metadata.xml.domain.impl.ResourceProperty;

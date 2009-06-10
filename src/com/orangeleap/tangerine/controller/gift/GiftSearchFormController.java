e5d615edfdf7c3580afd03c65e9c48b10a564dc2
tsporcic <tsporcic@0f581344-5646-3a48-8ded-616bfea576c6>
2009-06-10 19:17:33 +0000

    TANGERINE-669 Fixed to correctly handle global search and map the input field to the amount for searching.
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@6335 0f581344-5646-3a48-8ded-616bfea576c6

--- src/com/orangeleap/tangerine/controller/gift/GiftSearchFormController.java
+++ src/com/orangeleap/tangerine/controller/gift/GiftSearchFormController.java
@@ -1,9 +1,7 @@
 package com.orangeleap.tangerine.controller.gift;
 
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.math.BigDecimal;
 
 import javax.annotation.Resource;
 import javax.servlet.ServletException;
@@ -28,13 +26,15 @@ import com.orangeleap.tangerine.util.TangerinePagedListHolder;
 
 public class GiftSearchFormController extends SimpleFormController {
 
-    /** Logger for this class and subclasses */
+    /**
+     * Logger for this class and subclasses
+     */
     protected final Log logger = LogFactory.getLog(getClass());
 
-    @Resource(name="giftService")
+    @Resource(name = "giftService")
     private GiftService giftService;
 
-    @Resource(name="sessionService")
+    @Resource(name = "sessionService")
     private SessionService sessionService;
 
     @Override
@@ -55,20 +55,47 @@ public class GiftSearchFormController extends SimpleFormController {
         if (logger.isTraceEnabled()) {
             logger.trace("onSubmit:");
         }
+
         Gift gift = (Gift) command;
         BeanWrapper bw = PropertyAccessorFactory.forBeanPropertyAccess(gift);
         Map<String, Object> params = new HashMap<String, Object>();
+
         Enumeration<String> enu = request.getParameterNames();
         while (enu.hasMoreElements()) {
             String param = enu.nextElement();
-            if (!param.equalsIgnoreCase("page") && !param.equalsIgnoreCase("view") && !param.equalsIgnoreCase("sort") && StringUtils.trimToNull(request.getParameter(param)) != null) {
+            if (!param.equalsIgnoreCase("type") &&
+                !param.equalsIgnoreCase("page") &&
+                !param.equalsIgnoreCase("view") &&
+                !param.equalsIgnoreCase("sort") &&
+                StringUtils.trimToNull(request.getParameter(param)) != null) {
+
                 if (bw.isReadableProperty(param)){
                     params.put(param, bw.getPropertyValue(param));
                 }
             }
         }
 
+        // Search box is used for both constituent and gift search, the value we need
+        // is actually in an input field called lastName
+        String textValue = request.getParameter("lastName");
+
+        // if this is not null, it means they did a general search for Gift from the
+        // search box in the navigation bar
+        if (textValue != null) {
+            // Make sure we have a BigDecimal
+            BigDecimal val = null;
+            try {
+                val = new BigDecimal(textValue);
+                gift.setAmount(val);
+                params.put("amount", val);
+            } catch (NumberFormatException ex) {
+                // not a valid big decimal so ignore parameter
+                logger.info("Invalid BigDecimal for gift search [" + textValue + "]", ex);
+            }
+        }
+
         List<Gift> giftList = giftService.searchGifts(params);
+
         String sort = request.getParameter("sort");
         String ascending = request.getParameter("ascending");
         Boolean sortAscending;
@@ -77,8 +104,8 @@ public class GiftSearchFormController extends SimpleFormController {
         } else {
             sortAscending = new Boolean(true);
         }
-        MutableSortDefinition sortDef = new MutableSortDefinition(sort,true,sortAscending);
-        TangerinePagedListHolder pagedListHolder = new TangerinePagedListHolder(giftList,sortDef);
+        MutableSortDefinition sortDef = new MutableSortDefinition(sort, true, sortAscending);
+        TangerinePagedListHolder pagedListHolder = new TangerinePagedListHolder(giftList, sortDef);
         pagedListHolder.resort();
         pagedListHolder.setMaxLinkedPages(3);
         pagedListHolder.setPageSize(10);

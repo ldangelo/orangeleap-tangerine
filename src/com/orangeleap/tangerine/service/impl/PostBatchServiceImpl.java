0a79c6fee4d47c775fce42a5bea06fbeb095d400
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-06-23 22:09:45 +0000

    TANGERINE-844
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@6555 0f581344-5646-3a48-8ded-616bfea576c6

--- src/com/orangeleap/tangerine/service/impl/PostBatchServiceImpl.java
+++ src/com/orangeleap/tangerine/service/impl/PostBatchServiceImpl.java
@@ -2,16 +2,22 @@ package com.orangeleap.tangerine.service.impl;
 
 import com.orangeleap.tangerine.domain.PostBatch;
 import com.orangeleap.tangerine.domain.PostBatchReviewSetItem;
+import com.orangeleap.tangerine.domain.Journal;
 import com.orangeleap.tangerine.domain.customization.CustomField;
+import com.orangeleap.tangerine.domain.customization.Picklist;
+import com.orangeleap.tangerine.domain.customization.PicklistItem;
 import com.orangeleap.tangerine.domain.paymentInfo.Gift;
+import com.orangeleap.tangerine.domain.paymentInfo.DistributionLine;
 import com.orangeleap.tangerine.service.PostBatchService;
 import com.orangeleap.tangerine.service.GiftService;
 import com.orangeleap.tangerine.service.SiteService;
+import com.orangeleap.tangerine.service.PicklistItemService;
 import org.apache.commons.logging.Log;
 import com.orangeleap.tangerine.util.OLLogger;
 import com.orangeleap.tangerine.util.TangerineUserHelper;
 import com.orangeleap.tangerine.dao.PledgeDao;
 import com.orangeleap.tangerine.dao.PostBatchDao;
+import com.orangeleap.tangerine.dao.JournalDao;
 import com.orangeleap.tangerine.dao.util.QueryUtil;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -22,6 +28,7 @@ import org.springframework.beans.BeanWrapperImpl;
 
 import javax.annotation.Resource;
 import java.util.*;
+import java.math.BigDecimal;
 
 @Service("postBatchService")
 @Transactional(propagation = Propagation.REQUIRED)
@@ -39,6 +46,12 @@ public class PostBatchServiceImpl extends AbstractTangerineService implements Po
 
     @Resource(name = "siteService")
     private SiteService siteService;
+
+    @Resource(name = "journalDAO")
+    private JournalDao journalDao;
+
+    @Resource(name = "picklistItemService")
+    private PicklistItemService picklistItemService;
 
     @Resource(name="tangerineUserHelper")
     protected TangerineUserHelper tangerineUserHelper;
@@ -120,11 +133,42 @@ public class PostBatchServiceImpl extends AbstractTangerineService implements Po
              result.add(giftService.readGiftById(item.getEntityId()));
          }
          return result;
-    }                                    
+    }
+
+
+    private final static String ACCOUNT_STRING_1 = "AccountString1";
+    private final static String ACCOUNT_STRING_2 = "AccountString2";
 
     // Sets fields on gifts in reviewed batch list
     @Override
     public PostBatch postBatch(PostBatch postbatch) {
+
+        Picklist projectCodes = picklistItemService.getPicklist("projectCode");
+        if (projectCodes == null || projectCodes.getActivePicklistItems().size() == 0) {
+            throw new RuntimeException("Designation codes for GL account codes not defined.");
+        }
+        Map<String, String> codemap = new HashMap<String, String>();
+        for (PicklistItem item : projectCodes.getPicklistItems()) {
+            if (item.getCustomFieldValue(ACCOUNT_STRING_1) == null ||item.getCustomFieldValue(ACCOUNT_STRING_2) == null) {
+                throw new RuntimeException("GL account code not defined for designation code "+item.getDefaultDisplayValue());
+            }
+            codemap.put(item.getItemName() + ACCOUNT_STRING_1, item.getCustomFieldValue(ACCOUNT_STRING_1));
+            codemap.put(item.getItemName() + ACCOUNT_STRING_2, item.getCustomFieldValue(ACCOUNT_STRING_2));
+        }
+
+        Picklist bankCodes = picklistItemService.getPicklist("customFieldMap[bank]");
+        if (bankCodes == null || bankCodes.getActivePicklistItems().size() == 0) {
+            throw new RuntimeException("Bank GL account codes not defined.");
+        }
+        Map<String, String> bankmap = new HashMap<String, String>();
+        for (PicklistItem item : bankCodes.getPicklistItems()) {
+            if (item.getCustomFieldValue(ACCOUNT_STRING_1) == null ||item.getCustomFieldValue(ACCOUNT_STRING_2) == null) {
+                throw new RuntimeException("GL account code not defined for bank code "+item.getDefaultDisplayValue());
+            }
+            bankmap.put(item.getItemName() + ACCOUNT_STRING_1, item.getCustomFieldValue(ACCOUNT_STRING_1));
+            bankmap.put(item.getItemName() + ACCOUNT_STRING_2, item.getCustomFieldValue(ACCOUNT_STRING_2));
+        }
+
         postbatch.getUpdateErrors().clear();
         List<Gift> list = getBatchSelectionList(postbatch);
         for (Gift gift: list) {
@@ -134,6 +178,7 @@ public class PostBatchServiceImpl extends AbstractTangerineService implements Po
                 try {
                     bw.setPropertyValue(me.getKey(), me.getValue());    // TODO make work for dates
                     giftService.maintainGift(gift);
+                    createJournalEntries(gift, postbatch, codemap, bankmap);
                 } catch (BindException e) {
                     String msg = gift.getId() + ": " + e.getMessage();
                     logger.error(msg);
@@ -166,6 +211,42 @@ public class PostBatchServiceImpl extends AbstractTangerineService implements Po
             return errors;
         }
 
+    }
+
+    private void createJournalEntries(Gift gift, PostBatch postbatch, Map<String, String> codemap, Map<String, String> bankmap) {
+        createJournalEntry(gift, null, postbatch, codemap, bankmap);
+        for (DistributionLine dl : gift.getDistributionLines()) {
+            createJournalEntry(gift, dl, postbatch, codemap, bankmap);
+        }
+    }
+
+    private static String DEBIT = "debit";
+    private static String CREDIT = "credit";
+    private static String BANK = "bank";
+    private static String GIFT = "gift";
+
+    private void createJournalEntry(Gift gift, DistributionLine dl, PostBatch postbatch, Map<String, String> codemap, Map<String, String> bankmap) {
+        boolean isDebit = gift.getAmount().compareTo(new BigDecimal("0")) >= 0;
+        Journal journal = new Journal();
+        journal.setEntity(GIFT);
+        journal.setEntityId(gift.getId());
+        journal.setAmount(gift.getAmount());
+        journal.setSiteName(getSiteName());
+        journal.setPostedDate(new java.util.Date());
+        journal.setPostBatchId(postbatch.getId());
+        if (dl == null) {
+            journal.setJeType(isDebit?DEBIT:CREDIT);
+            String bank = gift.getCustomFieldValue(BANK);
+            journal.setGlCode1(bankmap.get(bank+ACCOUNT_STRING_1));
+            journal.setGlCode2(bankmap.get(bank+ACCOUNT_STRING_2));
+        } else {
+            journal.setJeType(isDebit?CREDIT:DEBIT);
+            journal.setGlCode1(codemap.get(dl.getProjectCode()+ACCOUNT_STRING_1));
+            journal.setGlCode2(codemap.get(dl.getProjectCode()+ACCOUNT_STRING_2));
+        }
+
+        journal.setDonationDate(gift.getDonationDate());
+        journalDao.maintainJournal(journal);
     }
 
 }

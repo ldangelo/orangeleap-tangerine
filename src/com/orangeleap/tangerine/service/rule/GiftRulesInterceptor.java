7f6a5d985a510c01cb90958140f91d6fa88b2ed4
ldangelo <ldangelo@0f581344-5646-3a48-8ded-616bfea576c6>
2009-07-08 20:39:06 +0000

    Updated copywrite information
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@6766 0f581344-5646-3a48-8ded-616bfea576c6

--- src/com/orangeleap/tangerine/service/rule/GiftRulesInterceptor.java
+++ src/com/orangeleap/tangerine/service/rule/GiftRulesInterceptor.java
@@ -1,7 +1,33 @@
+/*
+ * Copyright (c) 2009. Orange Leap Inc. Active Constituent
+ * Relationship Management Platform.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.orangeleap.tangerine.service.rule;
 
-import org.apache.commons.logging.Log;
+import com.orangeleap.tangerine.domain.Constituent;
+import com.orangeleap.tangerine.domain.paymentInfo.Gift;
+import com.orangeleap.tangerine.event.GiftEvent;
+import com.orangeleap.tangerine.event.NewGiftEvent;
+import com.orangeleap.tangerine.service.*;
 import com.orangeleap.tangerine.util.OLLogger;
+import com.orangeleap.tangerine.util.RuleTask;
+import com.orangeleap.tangerine.util.TangerineUserHelper;
+import com.orangeleap.tangerine.util.TaskStack;
+import org.apache.commons.logging.Log;
 import org.drools.RuleBase;
 import org.drools.StatefulSession;
 import org.drools.WorkingMemory;
@@ -12,122 +38,111 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationEvent;
 
-import com.orangeleap.tangerine.domain.Constituent;
-import com.orangeleap.tangerine.domain.paymentInfo.Gift;
-import com.orangeleap.tangerine.event.GiftEvent;
-import com.orangeleap.tangerine.event.NewGiftEvent;
-import com.orangeleap.tangerine.service.*;
-import com.orangeleap.tangerine.util.RuleTask;
-import com.orangeleap.tangerine.util.TangerineUserHelper;
-import com.orangeleap.tangerine.util.TaskStack;
-
 public class GiftRulesInterceptor extends RulesInterceptor {
 
-	private static final Log logger = OLLogger.getLog(GiftRulesInterceptor.class);
+    private static final Log logger = OLLogger.getLog(GiftRulesInterceptor.class);
 
-	private ApplicationContext applicationContext;
-	
-	
-	
-	public GiftRulesInterceptor(String droolsHost, String droolsPort) {
+    private ApplicationContext applicationContext;
+
+
+    public GiftRulesInterceptor(String droolsHost, String droolsPort) {
         super(droolsHost, droolsPort);
     }
-	
-	@Override
+
+    @Override
     public void doApplyRules(Gift gift) {
         String site = null;
-        site =gift.getSite().getName();
+        site = gift.getSite().getName();
 
-        RuleAgent agent = (RuleAgent) ((DroolsRuleAgent)applicationContext.getBean("DroolsRuleAgent")).getRuleAgent(site);
-		RuleBase ruleBase = agent.getRuleBase();
+        RuleAgent agent = (RuleAgent) ((DroolsRuleAgent) applicationContext.getBean("DroolsRuleAgent")).getRuleAgent(site);
+        RuleBase ruleBase = agent.getRuleBase();
 
-		StatefulSession session = ruleBase.newStatefulSession();
-		WorkingMemory workingMemory = (WorkingMemory) session;
-		
-		if (logger.isDebugEnabled()) {
-			workingMemory.addEventListener (new DebugAgendaEventListener());
-			workingMemory.addEventListener(new DebugWorkingMemoryEventListener());
-		}
-		
-		
-		@SuppressWarnings("unused")
-		ConstituentService ps = (ConstituentService) applicationContext.getBean("constituentService");
-		GiftService gs = (GiftService) applicationContext.getBean("giftService");
-		SiteService ss = (SiteService) applicationContext.getBean("siteService");
-		TangerineUserHelper uh = (TangerineUserHelper) applicationContext.getBean("tangerineUserHelper");
+        StatefulSession session = ruleBase.newStatefulSession();
+        WorkingMemory workingMemory = (WorkingMemory) session;
+
+        if (logger.isDebugEnabled()) {
+            workingMemory.addEventListener(new DebugAgendaEventListener());
+            workingMemory.addEventListener(new DebugWorkingMemoryEventListener());
+        }
+
+
+        @SuppressWarnings("unused")
+        ConstituentService ps = (ConstituentService) applicationContext.getBean("constituentService");
+        GiftService gs = (GiftService) applicationContext.getBean("giftService");
+        SiteService ss = (SiteService) applicationContext.getBean("siteService");
+        TangerineUserHelper uh = (TangerineUserHelper) applicationContext.getBean("tangerineUserHelper");
         EmailService es = (EmailService) applicationContext.getBean("emailService");
-		ErrorLogService errorLogService = (ErrorLogService) applicationContext.getBean("errorLogService");
-		
+        ErrorLogService errorLogService = (ErrorLogService) applicationContext.getBean("errorLogService");
 
 
-			try {
+        try {
 
-				workingMemory.setFocus(site+"gift");
-				workingMemory.setGlobal("applicationContext", applicationContext);
-				workingMemory.setGlobal("constituentService", ps);
-				workingMemory.setGlobal("giftService",gs);
-				workingMemory.setGlobal("userHelper",uh);
-				
-				workingMemory.insert(ss.readSite(site));
-				
-				ss.populateDefaultEntityEditorMaps(gift);
-				workingMemory.insert(gift);
-				
-				/*List<Gift> gifts = gs.readMonetaryGiftsByConstituentId(gift.getConstituent().getId()); 
-				Iterator<Gift> giftsIter = gifts.iterator();
-				while (giftsIter.hasNext()) {
-					workingMemory.insert(giftsIter.next());
-				}*/
+            workingMemory.setFocus(site + "gift");
+            workingMemory.setGlobal("applicationContext", applicationContext);
+            workingMemory.setGlobal("constituentService", ps);
+            workingMemory.setGlobal("giftService", gs);
+            workingMemory.setGlobal("userHelper", uh);
 
-				Constituent constituent = gift.getConstituent();
-				
-				constituent.setGifts(gs.readMonetaryGifts(constituent));
-				constituent.setSite(ss.readSite(constituent.getSite().getName()));
-				constituent.setEmails(es.readByConstituentId(constituent.getId()));
-				ss.populateDefaultEntityEditorMaps(constituent);
-				constituent.setSuppressValidation(true);
+            workingMemory.insert(ss.readSite(site));
 
-				TaskStack.push(new RuleTask(applicationContext,site + "email",constituent,gift));
+            ss.populateDefaultEntityEditorMaps(gift);
+            workingMemory.insert(gift);
 
-				workingMemory.insert(constituent);
+            /*List<Gift> gifts = gs.readMonetaryGiftsByConstituentId(gift.getConstituent().getId());
+                   Iterator<Gift> giftsIter = gifts.iterator();
+                   while (giftsIter.hasNext()) {
+                       workingMemory.insert(giftsIter.next());
+                   }*/
 
-			} catch (Exception ex) {
-				logger.error(ex.getMessage());
-				errorLogService.addErrorMessage(ex.getMessage(), "gift.rule.setup");
-			}
+            Constituent constituent = gift.getConstituent();
 
-			if (site == null) {
-				site =gift.getSite().getName();
-			}
+            constituent.setGifts(gs.readMonetaryGifts(constituent));
+            constituent.setSite(ss.readSite(constituent.getSite().getName()));
+            constituent.setEmails(es.readByConstituentId(constituent.getId()));
+            ss.populateDefaultEntityEditorMaps(constituent);
+            constituent.setSuppressValidation(true);
 
-		try {
-			logger.info("*** firing all rules");
+            TaskStack.push(new RuleTask(applicationContext, site + "email", constituent, gift));
 
-			workingMemory.fireAllRules();
-			
-			
-			session.dispose();
-			workingMemory = null;
+            workingMemory.insert(constituent);
 
-		} catch (Exception e) {
-			logger.error("*** exception firing rules - make sure rule base exists and global variable is set: ");
-			logger.error(e);
-			errorLogService.addErrorMessage(e.getMessage(), "gift.rule.fire");
-		}
-	}
+        } catch (Exception ex) {
+            logger.error(ex.getMessage());
+            errorLogService.addErrorMessage(ex.getMessage(), "gift.rule.setup");
+        }
 
-	@Override
-	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-		this.applicationContext = applicationContext;
-	}
+        if (site == null) {
+            site = gift.getSite().getName();
+        }
+
+        try {
+            logger.info("*** firing all rules");
+
+            workingMemory.fireAllRules();
 
 
-	@Override
-	public void onApplicationEvent(ApplicationEvent event) {
-		if (event instanceof GiftEvent) {
-			NewGiftEvent nge = (NewGiftEvent) event;
-			doApplyRules(nge.getGift());
-		}
-		
-	}
+            session.dispose();
+            workingMemory = null;
+
+        } catch (Exception e) {
+            logger.error("*** exception firing rules - make sure rule base exists and global variable is set: ");
+            logger.error(e);
+            errorLogService.addErrorMessage(e.getMessage(), "gift.rule.fire");
+        }
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
+        if (event instanceof GiftEvent) {
+            NewGiftEvent nge = (NewGiftEvent) event;
+            doApplyRules(nge.getGift());
+        }
+
+    }
 }
--- src/com/orangeleap/tangerine/service/rule/RulesInterceptor.java
+++ src/com/orangeleap/tangerine/service/rule/RulesInterceptor.java
@@ -1,15 +1,33 @@
+/*
+ * Copyright (c) 2009. Orange Leap Inc. Active Constituent
+ * Relationship Management Platform.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.orangeleap.tangerine.service.rule;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.Properties;
-
-import org.apache.commons.logging.Log;
+import com.orangeleap.tangerine.domain.Constituent;
+import com.orangeleap.tangerine.domain.paymentInfo.Gift;
+import com.orangeleap.tangerine.service.ConstituentService;
+import com.orangeleap.tangerine.service.GiftService;
+import com.orangeleap.tangerine.service.SiteService;
 import com.orangeleap.tangerine.util.OLLogger;
+import org.apache.commons.logging.Log;
 import org.drools.RuleBase;
 import org.drools.StatefulSession;
 import org.drools.WorkingMemory;
-import org.drools.agent.RuleAgent;
 import org.drools.event.DebugAgendaEventListener;
 import org.drools.event.DebugWorkingMemoryEventListener;
 import org.springframework.beans.BeansException;
@@ -17,31 +35,24 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ApplicationListener;
 
-import com.orangeleap.tangerine.domain.Constituent;
-import com.orangeleap.tangerine.domain.paymentInfo.Gift;
-import com.orangeleap.tangerine.service.ConstituentService;
-import com.orangeleap.tangerine.service.GiftService;
-import com.orangeleap.tangerine.service.SiteService;
-import com.orangeleap.tangerine.util.RuleTask;
-import com.orangeleap.tangerine.util.TaskStack;
-
+import java.util.Properties;
 
 
 public abstract class RulesInterceptor implements ApplicationContextAware, ApplicationListener {
 
-	private static final Log logger = OLLogger.getLog(RulesInterceptor.class);
+    private static final Log logger = OLLogger.getLog(RulesInterceptor.class);
 
-	private ApplicationContext applicationContext;
-	private String agendaGroup;
-	@SuppressWarnings("unchecked")
-    private Class  eventClass;
-	private final Properties droolsProperties;
-	
-	public RulesInterceptor() {
-		droolsProperties = new Properties();
-	}
-	
-	public RulesInterceptor(final String droolsHost, final String droolsPort) {
+    private ApplicationContext applicationContext;
+    private String agendaGroup;
+    @SuppressWarnings("unchecked")
+    private Class eventClass;
+    private final Properties droolsProperties;
+
+    public RulesInterceptor() {
+        droolsProperties = new Properties();
+    }
+
+    public RulesInterceptor(final String droolsHost, final String droolsPort) {
         if (logger.isDebugEnabled()) {
             logger.debug("RulesInterceptor: droolsHost = " + droolsHost + " droolsPort = " + droolsPort);
         }
@@ -50,99 +61,99 @@ public abstract class RulesInterceptor implements ApplicationContextAware, Appli
         logger.debug("Setting Drools URL to " + url);
         droolsProperties.put("url", url);
         droolsProperties.put("newInstance", "true");
-        droolsProperties.put("name","testagent");
+        droolsProperties.put("name", "testagent");
     }
 
-	public Properties getDroolsProperties() {
+    public Properties getDroolsProperties() {
         return droolsProperties;
     }
 
     public void doApplyRules(Gift gift) {
 
-		RuleBase ruleBase = ((DroolsRuleAgent)applicationContext.getBean("DroolsRuleAgent")).getRuleAgent(gift.getSite().getName()).getRuleBase();
+        RuleBase ruleBase = ((DroolsRuleAgent) applicationContext.getBean("DroolsRuleAgent")).getRuleAgent(gift.getSite().getName()).getRuleBase();
 
-		StatefulSession session = ruleBase.newStatefulSession();
-		WorkingMemory workingMemory = (WorkingMemory) session;
-		
-		if (logger.isDebugEnabled()) {
-			workingMemory.addEventListener (new DebugAgendaEventListener());
-			workingMemory.addEventListener(new DebugWorkingMemoryEventListener());
-		}
-		
-		
-		@SuppressWarnings("unused")
-		ConstituentService ps = (ConstituentService) applicationContext.getBean("constituentService");
-		GiftService gs = (GiftService) applicationContext.getBean("giftService");
-		SiteService ss = (SiteService) applicationContext.getBean("siteService");
-		
-		String site = null;
+        StatefulSession session = ruleBase.newStatefulSession();
+        WorkingMemory workingMemory = (WorkingMemory) session;
 
-		workingMemory.setGlobal("applicationContext", applicationContext);
-		workingMemory.setGlobal("constituentService", ps);
-		workingMemory.setGlobal("giftService",gs);
+        if (logger.isDebugEnabled()) {
+            workingMemory.addEventListener(new DebugAgendaEventListener());
+            workingMemory.addEventListener(new DebugWorkingMemoryEventListener());
+        }
 
-			try {
-				if (site == null) {
-					site =gift.getSite().getName();
-				}
-				workingMemory.setFocus(site+"email");
-				
-				workingMemory.insert(gift);
-				
-				Constituent constituent = gift.getConstituent();
-				
-				constituent.setGifts(gs.readMonetaryGifts(constituent));
-				constituent.setSite(ss.readSite(constituent.getSite().getName()));
 
-				workingMemory.insert(constituent);
+        @SuppressWarnings("unused")
+        ConstituentService ps = (ConstituentService) applicationContext.getBean("constituentService");
+        GiftService gs = (GiftService) applicationContext.getBean("giftService");
+        SiteService ss = (SiteService) applicationContext.getBean("siteService");
 
-			} catch (Exception ex) {
-				logger.error(ex.getMessage());
-			}
+        String site = null;
 
-			if (site == null) {
-				site =gift.getSite().getName();
-			}
+        workingMemory.setGlobal("applicationContext", applicationContext);
+        workingMemory.setGlobal("constituentService", ps);
+        workingMemory.setGlobal("giftService", gs);
 
-		try {
+        try {
+            if (site == null) {
+                site = gift.getSite().getName();
+            }
+            workingMemory.setFocus(site + "email");
 
-			logger.info("*** firing all rules");
+            workingMemory.insert(gift);
 
-			workingMemory.fireAllRules();
-			
-			
-			session.dispose();
-			workingMemory = null;
+            Constituent constituent = gift.getConstituent();
 
-		} catch (Exception e) {
-			logger.error("*** exception firing rules - make sure rule base exists and global variable is set: ");
-			logger.error(e);
-		}
-	}
+            constituent.setGifts(gs.readMonetaryGifts(constituent));
+            constituent.setSite(ss.readSite(constituent.getSite().getName()));
 
-	@Override
-	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-		this.applicationContext = applicationContext;
-	}
-	
-	public ApplicationContext getApplicationContext() {
-			return this.applicationContext;
-	}
+            workingMemory.insert(constituent);
 
-	public String getAgendaGroup() {
-		return agendaGroup;
-	}
+        } catch (Exception ex) {
+            logger.error(ex.getMessage());
+        }
 
-	public void setAgendaGroup(String agendaGroup) {
-		this.agendaGroup = agendaGroup;
-	}
+        if (site == null) {
+            site = gift.getSite().getName();
+        }
 
-	/*@Override
-	public void onApplicationEvent(ApplicationEvent event) {
-		if (event.getClass() == eventClass) {
-			NewGiftEvent nge = (NewGiftEvent) event;
-			doApplyRules(nge.getGift());
-		}
-		
-	}*/
+        try {
+
+            logger.info("*** firing all rules");
+
+            workingMemory.fireAllRules();
+
+
+            session.dispose();
+            workingMemory = null;
+
+        } catch (Exception e) {
+            logger.error("*** exception firing rules - make sure rule base exists and global variable is set: ");
+            logger.error(e);
+        }
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    public ApplicationContext getApplicationContext() {
+        return this.applicationContext;
+    }
+
+    public String getAgendaGroup() {
+        return agendaGroup;
+    }
+
+    public void setAgendaGroup(String agendaGroup) {
+        this.agendaGroup = agendaGroup;
+    }
+
+    /*@Override
+     public void onApplicationEvent(ApplicationEvent event) {
+         if (event.getClass() == eventClass) {
+             NewGiftEvent nge = (NewGiftEvent) event;
+             doApplyRules(nge.getGift());
+         }
+
+     }*/
 }

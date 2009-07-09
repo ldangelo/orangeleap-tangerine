a911c98d7c767c65a27688d2e4edfcf8a5bc5c28
alo <alo@0f581344-5646-3a48-8ded-616bfea576c6>
2009-07-09 22:22:16 +0000

    changes roll back
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@6797 0f581344-5646-3a48-8ded-616bfea576c6

---
+++ src/com/orangeleap/tangerine/service/impl/PaymentHistoryServiceImpl.java
@@ -0,0 +1,52 @@
+package com.orangeleap.tangerine.service.impl;
+
+import com.orangeleap.tangerine.dao.PaymentHistoryDao;
+import com.orangeleap.tangerine.domain.PaymentHistory;
+import com.orangeleap.tangerine.service.PaymentHistoryService;
+import com.orangeleap.tangerine.util.OLLogger;
+import com.orangeleap.tangerine.web.common.PaginatedResult;
+import com.orangeleap.tangerine.web.common.SortInfo;
+import org.apache.commons.logging.Log;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+
+@Service("paymentHistoryService")
+@Transactional(propagation = Propagation.REQUIRED)
+public class PaymentHistoryServiceImpl extends AbstractTangerineService implements PaymentHistoryService {
+
+    /** Logger for this class and subclasses */
+    protected final Log logger = OLLogger.getLog(getClass());
+
+    @Resource(name = "paymentHistoryDAO")
+    private PaymentHistoryDao paymentHistoryDao;
+
+	@Override
+	public PaymentHistory addPaymentHistory(PaymentHistory paymentHistory) {
+        if (logger.isTraceEnabled()) {
+            logger.trace("addPaymentHistory: paymentHistory = " + paymentHistory);
+        }
+		if (paymentHistory.getConstituent() == null) {
+            return null;
+        }
+		return paymentHistoryDao.addPaymentHistory(paymentHistory);
+	}
+
+	@Override
+	public PaginatedResult readPaymentHistory(Long constituentId, SortInfo sortinfo) {
+        if (logger.isTraceEnabled()) {
+            logger.trace("readPaymentHistory: constituentId = " + constituentId);
+        }
+		return paymentHistoryDao.readPaymentHistoryByConstituentId(constituentId, sortinfo);
+	}
+
+	@Override
+	public PaginatedResult readPaymentHistoryBySite(SortInfo sortinfo) {
+        if (logger.isTraceEnabled()) {
+            logger.trace("readPaymentHistoryBySite:");
+        }
+		return paymentHistoryDao.readPaymentHistoryBySite(sortinfo);
+	}
+}

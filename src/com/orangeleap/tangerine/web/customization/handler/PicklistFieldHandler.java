90ad8098af5869bec623daccccff536abff1fa8c
alo <alo@0f581344-5646-3a48-8ded-616bfea576c6>
2009-07-10 15:43:20 +0000

    TANGERINE-1004: show long description if available, else show default display value
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@6802 0f581344-5646-3a48-8ded-616bfea576c6

--- src/com/orangeleap/tangerine/web/customization/handler/PicklistFieldHandler.java
+++ src/com/orangeleap/tangerine/web/customization/handler/PicklistFieldHandler.java
@@ -18,21 +18,20 @@
 
 package com.orangeleap.tangerine.web.customization.handler;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.Locale;
-
-import org.apache.commons.logging.Log;
-import com.orangeleap.tangerine.util.OLLogger;
-import org.apache.commons.validator.GenericValidator;
-import org.springframework.context.ApplicationContext;
-
 import com.orangeleap.tangerine.domain.customization.Picklist;
 import com.orangeleap.tangerine.domain.customization.PicklistItem;
 import com.orangeleap.tangerine.domain.customization.SectionField;
 import com.orangeleap.tangerine.type.EntityType;
 import com.orangeleap.tangerine.type.MessageResourceType;
+import com.orangeleap.tangerine.util.OLLogger;
 import com.orangeleap.tangerine.web.customization.FieldVO;
+import org.apache.commons.logging.Log;
+import org.apache.commons.validator.GenericValidator;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
 
 public class PicklistFieldHandler extends GenericFieldHandler {
 
@@ -54,7 +53,11 @@ public class PicklistFieldHandler extends GenericFieldHandler {
                 fieldVO.addCode(item.getItemName());
                 String displayValue = messageService.lookupMessage(MessageResourceType.PICKLIST_VALUE, item.getItemName(), locale);
                 if (GenericValidator.isBlankOrNull(displayValue)) {
-                    displayValue = item.getDefaultDisplayValue();
+                    displayValue = item.getLongDescription();
+
+	                if (GenericValidator.isBlankOrNull(displayValue)) {
+		                displayValue = item.getDefaultDisplayValue();
+	                }
                 }
                 fieldVO.addDisplayValue(displayValue);
                 fieldVO.addReferenceValue(item.getReferenceValue());

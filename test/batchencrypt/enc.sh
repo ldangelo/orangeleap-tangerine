e193c73f2ef0e567d04db50f8abae4a6f56c6d98
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-07-20 18:37:38 +0000

    batch encryption script
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@6914 0f581344-5646-3a48-8ded-616bfea576c6

---
+++ test/batchencrypt/enc.sh
@@ -0,0 +1,3 @@
+export LIB=/opt/orangeleap/instance_DEV/webapps/orangeleap/WEB-INF/lib
+java -cp "$LIB/commons-codec-1.3.jar:$LIB/commons-io-1.2.jar:$LIB/tangerine.jar" com.orangeleap.tangerine.util.AESEncrypt $1 $2
+

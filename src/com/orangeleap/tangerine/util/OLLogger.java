5a5428c8378c0cc1dc23c160573c75666ce6ac44
ldangelo <ldangelo@0f581344-5646-3a48-8ded-616bfea576c6>
2009-07-21 21:57:46 +0000

    added copyright
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@6940 0f581344-5646-3a48-8ded-616bfea576c6

--- src/com/orangeleap/tangerine/util/OLLogger.java
+++ src/com/orangeleap/tangerine/util/OLLogger.java
@@ -1,14 +1,31 @@
+/*
+ * Copyright (c) 2009. Orange Leap Inc. Active Constituent
+ * Relationship Management Platform.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.orangeleap.tangerine.util;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import com.orangeleap.tangerine.util.OLLogger;
 
 public class OLLogger implements Log, java.io.Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private static boolean enableTableLogging = ( null == System.getProperty("ol.disable.table.logging"));
+    private static boolean enableTableLogging = (null == System.getProperty("ol.disable.table.logging"));
 
     private static TableLogger tablelogger;
 
@@ -130,7 +147,7 @@ public class OLLogger implements Log, java.io.Serializable {
     }
 
     private void logToTable(String loglevel, Object o, Throwable t) {
-       if (tablelogger != null && enableTableLogging) tablelogger.logToTable(loglevel,  o,  t);
+        if (tablelogger != null && enableTableLogging) tablelogger.logToTable(loglevel, o, t);
     }
 
 }

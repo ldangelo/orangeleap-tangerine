102661ed0101a6a326352865871085032e7f0007
ldangelo <ldangelo@0f581344-5646-3a48-8ded-616bfea576c6>
2009-07-21 21:54:13 +0000

    added ability to send an email to an arbitrary list of e-mail addresses
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@6938 0f581344-5646-3a48-8ded-616bfea576c6

--- src/com/orangeleap/tangerine/service/communication/EmailService.java
+++ src/com/orangeleap/tangerine/service/communication/EmailService.java
@@ -18,35 +18,6 @@
 
 package com.orangeleap.tangerine.service.communication;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-
-import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JasperExportManager;
-import net.sf.jasperreports.engine.JasperPrint;
-
-import org.apache.commons.logging.Log;
-import com.orangeleap.tangerine.util.OLLogger;
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.validation.BindException;
-
 import com.jaspersoft.jasperserver.api.metadata.xml.domain.impl.ResourceDescriptor;
 import com.jaspersoft.jasperserver.api.metadata.xml.domain.impl.ResourceProperty;
 import com.jaspersoft.jasperserver.irplugin.JServer;
@@ -58,285 +29,320 @@ import com.orangeleap.tangerine.domain.Site;
 import com.orangeleap.tangerine.domain.communication.Email;
 import com.orangeleap.tangerine.domain.paymentInfo.Gift;
 import com.orangeleap.tangerine.service.CommunicationHistoryService;
+import com.orangeleap.tangerine.util.OLLogger;
 import com.orangeleap.tangerine.util.TangerineUserHelper;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import org.apache.commons.logging.Log;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.validation.BindException;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.lang.reflect.Field;
+import java.util.*;
 
 //@Service("emailSendingService")
 public class EmailService implements ApplicationContextAware {
-	protected final Log logger = OLLogger.getLog(getClass());
-	private JServer jserver = null;
-	private String userName = null;
-	private String password = null;
-	private String uri = null;
-	private String templateName = null;
-	private String subject = null;
-	private CommunicationHistoryService communicationHistoryService;
-	private java.util.Map map = new HashMap();
-	private Site site;
-	private ApplicationContext applicationContext;
-	private JasperPrint print;
+    protected final Log logger = OLLogger.getLog(getClass());
+    private JServer jserver = null;
+    private String userName = null;
+    private String password = null;
+    private String uri = null;
+    private String templateName = null;
+    private String subject = null;
+    private CommunicationHistoryService communicationHistoryService;
+    private java.util.Map map = new HashMap();
+    private Site site;
+    private ApplicationContext applicationContext;
+    private JasperPrint print;
 
-	private File runReport() {
-		File temp = null;
-		TangerineUserHelper tuh = (TangerineUserHelper) applicationContext.getBean("tangerineUserHelper");
-		jserver = new JServer();
+    private File runReport() {
+        File temp = null;
+        TangerineUserHelper tuh = (TangerineUserHelper) applicationContext.getBean("tangerineUserHelper");
+        jserver = new JServer();
 //		jserver.setUsername(tuh.lookupUserName() + "@" + site.getName());
-		jserver.setUsername(site.getJasperUserId());
+        jserver.setUsername(site.getJasperUserId());
 //		jserver.setPassword(tuh.lookupUserPassword());
-		jserver.setPassword(site.getJasperPassword());
-		jserver.setUrl(uri);
-		try {
-			WSClient client = jserver.getWSClient();
+        jserver.setPassword(site.getJasperPassword());
+        jserver.setUrl(uri);
+        try {
+            WSClient client = jserver.getWSClient();
 
-			Map params = getReportParameters();
+            Map params = getReportParameters();
 
-			print = getServer().getWSClient().runReport(
-					getReportUnit().getDescriptor(), params);
+            print = getServer().getWSClient().runReport(
+                    getReportUnit().getDescriptor(), params);
 
-			temp = File.createTempFile("orangeleap", ".pdf");
-			temp.deleteOnExit();
-			OutputStream out= new FileOutputStream(temp);
-			JasperExportManager.exportReportToPdfStream(print,out);
-			out.close();
-			
-		} catch (JRException e) {
+            temp = File.createTempFile("orangeleap", ".pdf");
+            temp.deleteOnExit();
+            OutputStream out = new FileOutputStream(temp);
+            JasperExportManager.exportReportToPdfStream(print, out);
+            out.close();
 
-			logger.error(e.getMessage());
-			return null;
-		} catch (Exception e) {
-			logger.error(e.getMessage());
-			return null;
-		}
-		return temp;
-	}
+        } catch (JRException e) {
 
-	@SuppressWarnings("unchecked")
-	private void setParameters(Constituent p, Gift g) 
-	{
-		Class c = p.getClass();
-		Field[] fields = c.getDeclaredFields();
-		for (Field f: fields) {
-			try {
-				map.put(f.getName(),f.get(p).toString());
-			} catch (IllegalArgumentException e) {
-				logger.error(e.getMessage());
-			} catch (IllegalAccessException e) {
-				logger.error(e.getMessage());				
-			}
-		}
-		
-		c = g.getClass();
-		fields = c.getDeclaredFields();
-		for (Field f: fields) {
-			try {
-				map.put(f.getName(),f.get(p).toString());
-			} catch (IllegalArgumentException e) {
-				logger.error(e.getMessage());
-			} catch (IllegalAccessException e) {
-				logger.error(e.getMessage());
-			}
-		}		
-	}
-	
-	public void sendMail(Constituent p, Gift g, String subject, String templateName) {
-		List<Email> selectedEmails = new LinkedList<Email>();
-		setSubject(subject);
-		setTemplateName(templateName);
-		
-		Site s = p.getSite();
-		setSite(s);
-		
+            logger.error(e.getMessage());
+            return null;
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return null;
+        }
+        return temp;
+    }
 
-		//
-		// first we run the report passing in the constituent.id as a parameter
-		Map params = getReportParameters();
-		params.clear();
+    @SuppressWarnings("unchecked")
+    private void setParameters(Constituent p, Gift g) {
+        Class c = p.getClass();
+        Field[] fields = c.getDeclaredFields();
+        for (Field f : fields) {
+            try {
+                map.put(f.getName(), f.get(p).toString());
+            } catch (IllegalArgumentException e) {
+                logger.error(e.getMessage());
+            } catch (IllegalAccessException e) {
+                logger.error(e.getMessage());
+            }
+        }
 
-		
+        c = g.getClass();
+        fields = c.getDeclaredFields();
+        for (Field f : fields) {
+            try {
+                map.put(f.getName(), f.get(p).toString());
+            } catch (IllegalArgumentException e) {
+                logger.error(e.getMessage());
+            } catch (IllegalAccessException e) {
+                logger.error(e.getMessage());
+            }
+        }
+    }
 
-		params.put("Id",p.getId().toString());
-		params.put("GiftId", g.getId().toString());
-		params.put("GiftAmount", g.getAmount().toString());
+    public void sendMail(String addresses, Constituent p, Gift g, String subject, String templateName) {
+        setSubject(subject);
+        setTemplateName(templateName);
 
-		
+        Site s = g.getSite();
+        setSite(s);
 
-		//
-		// next we extract the output of the report and put it into a mime
-		// message
-		JavaMailSenderImpl sender = new JavaMailSenderImpl();
-		sender.setHost(site.getSmtpServerName());
-		
-		if (site.getSmtpAccountName() != null)
-			sender.setUsername(site.getSmtpAccountName());
-		if (site.getSmtpPassword() != null)
-			sender.setPassword(site.getSmtpPassword());
-		
-		MimeMessage message = sender.createMimeMessage();
-		MimeMessageHelper helper;
-		try {
-			helper = new MimeMessageHelper(message,true,"UTF-8");
-		} catch (MessagingException e2) {
-			logger.error(e2.getMessage());
-			return;
-		}
-		List<Email> emailAddresses = p.getEmails();
-		
-		for (Email e : emailAddresses) {
-			if (e.isReceiveCorrespondence() && !e.isInactive() && e.isValid()) {
-				selectedEmails.add(e);
-				try {
-					helper.addTo(e.getEmailAddress());
-				} catch (MessagingException e1) {
-					logger.error(e1.getMessage());
-				}
-			}
-		}
-		
-		//
-		// no e-mail addresses can receive mail
-		if (selectedEmails.size() == 0) {
-			return;
-		}
-		File tempFile = runReport();
-		
-		FileSystemResource file = new FileSystemResource(tempFile);
-		try {
-			helper.addAttachment(getTemplateName() + ".pdf",file);
-			helper.setText("Thank you for your recent donation!");
-			helper.setSubject(subject);
-			helper.setFrom(site.getSmtpFromAddress());
-			//
-			// finally we mail the message
-			sender.send(message);
+        Map params = getReportParameters();
+        params.clear();
 
-			//
-			// add entry to touchpoints for this e-mail
-			for (Email e: selectedEmails) {
-				CommunicationHistory ch = new CommunicationHistory();
-				ch.setConstituent(p);
-				ch.setGiftId(g.getId());
-				ch.setSystemGenerated(true);
-				ch.setComments("Sent e-mail using template named " + getTemplateName());
-				ch.setEntryType("Email");
-				ch.setRecordDate(new Date());
-				ch.setSelectedEmail(e);
-				ch.setCustomFieldValue("template", getTemplateName());
 
-				ch.setSuppressValidation(true);
-				try {
-					communicationHistoryService.maintainCommunicationHistory(ch);
-				} catch (BindException e1) {
-					// Should not happen when setSuppressValidation = true;
-					logger.error(e1);
-				}
-			
-			}
-			tempFile.delete();
-		} catch (MessagingException e1) {
-			logger.error(e1.getMessage());
-			return;
-		}
+        params.put("Id", p.getId().toString());
+        params.put("GiftId", g.getId().toString());
+        params.put("GiftAmount", g.getAmount().toString());
 
-	}
 
-	private void setTemplateName(String templateName) {
-		this.templateName = templateName;
-		
-	}
-	
-	private String getTemplateName()
-	{
-		return this.templateName;
-	}
+        //
+        // next we extract the output of the report and put it into a mime
+        // message
+        JavaMailSenderImpl sender = new JavaMailSenderImpl();
+        sender.setHost(site.getSmtpServerName());
 
-	private Map getReportParameters() {
+        if (site.getSmtpAccountName() != null)
+            sender.setUsername(site.getSmtpAccountName());
+        if (site.getSmtpPassword() != null)
+            sender.setPassword(site.getSmtpPassword());
 
-		return map;
-	}
+        MimeMessage message = sender.createMimeMessage();
+        MimeMessageHelper helper;
+        try {
+            helper = new MimeMessageHelper(message, true, "UTF-8");
+        } catch (MessagingException e2) {
+            logger.error(e2.getMessage());
+            return;
+        }
 
-	private RepositoryReportUnit getReportUnit() 
-	{
-			ResourceDescriptor rd = new ResourceDescriptor();
-			
-			rd.setName(this.getTemplateName());
-			rd.setParentFolder("/Reports/" + getSite().getName() + "/emailTemplates");
-			rd.setUriString(rd.getParentFolder() + "/" + rd.getName());
-			rd.setWsType(ResourceDescriptor.TYPE_REPORTUNIT);
-			List<ResourceProperty> p = new ArrayList<ResourceProperty>();
-			
-			Iterator it = map.entrySet().iterator();
-			while (it.hasNext()) {
-				Map.Entry me = (Map.Entry) it.next();
-			
-				ResourceProperty rp = new ResourceProperty(me.getKey().toString(),me.getValue().toString());
-				p.add(rp);
-			}
-			
-			rd.setParameters(p);
-			
-			return new RepositoryReportUnit(getServer(),rd);
-	}
+        File tempFile = runReport();
 
-	private JServer getServer() {
-		return jserver;
-	}
+        FileSystemResource file = new FileSystemResource(tempFile);
+        try {
+            helper.addAttachment(getTemplateName() + ".pdf", file);
+            helper.setText("Thank you for your recent donation!");
+            helper.setSubject(subject);
+            helper.setFrom(site.getSmtpFromAddress());
 
-	public String getUserName() {
-		return userName;
-	}
+            String emailAddresses[] = addresses.split(",");
+            for (int i = 0; i < emailAddresses.length; i++)
+                helper.addTo(emailAddresses[i]);
 
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
+            //
+            // finally we mail the message
+            sender.send(message);
 
-	public String getPassword() {
-		return password;
-	}
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+            tempFile.delete();
+        } catch (MessagingException e1) {
+            logger.error(e1.getMessage());
+            return;
+        }
 
-	public String getUri() {
-		return uri;
-	}
 
-	public void setUri(String uri) {
-		this.uri = uri;
-	}
+    }
 
-	public Log getLogger() {
-		return logger;
-	}
+    public void sendMail(Constituent p, Gift g, String subject, String templateName) {
+        String strEmailAddrs = "";
+        List<Email> selectedEmails = new LinkedList<Email>();
+        setSubject(subject);
+        setTemplateName(templateName);
 
-	public Site getSite() {
-		return site;
-	}
+        Site s = p.getSite();
+        setSite(s);
 
-	public void setSite(Site site) {
-		this.site = site;
-	}
 
-	public String getSubject() {
-		return subject;
-	}
+        //
+        // first we run the report passing in the constituent.id as a parameter
+        List<Email> emailAddresses = p.getEmails();
 
-	public void setSubject(String subject) {
-		this.subject = subject;
-	}
+        for (Email e : emailAddresses) {
+            if (e.isReceiveCorrespondence() && !e.isInactive() && e.isValid()) {
+                selectedEmails.add(e);
+                strEmailAddrs += e.getEmailAddress() + ",";
+            }
+        }
 
-	public CommunicationHistoryService getCommunicationHistoryService() {
-		return communicationHistoryService;
-	}
+        // remove the trailing ,
+        strEmailAddrs = strEmailAddrs.substring(0, strEmailAddrs.lastIndexOf(","));
 
-	public void setCommunicationHistoryService(
-			CommunicationHistoryService communicationHistoryService) {
-		this.communicationHistoryService = communicationHistoryService;
-	}
+        //
+        // no e-mail addresses can receive mail
+        if (selectedEmails.size() == 0) {
+            return;
+        }
 
-	@Override
-	public void setApplicationContext(ApplicationContext applicationContext)
-			throws BeansException {
-		this.applicationContext = applicationContext;
-		
-	}
+        this.sendMail(strEmailAddrs, p, g, subject, templateName);
+
+        //
+        // add entry to touchpoints for this e-mail
+        for (Email e : selectedEmails) {
+            CommunicationHistory ch = new CommunicationHistory();
+            ch.setConstituent(p);
+            ch.setGiftId(g.getId());
+            ch.setSystemGenerated(true);
+            ch.setComments("Sent e-mail using template named " + getTemplateName());
+            ch.setEntryType("Email");
+            ch.setRecordDate(new Date());
+            ch.setSelectedEmail(e);
+            ch.setCustomFieldValue("template", getTemplateName());
+
+            ch.setSuppressValidation(true);
+            try {
+                communicationHistoryService.maintainCommunicationHistory(ch);
+            } catch (BindException e1) {
+                // Should not happen when setSuppressValidation = true;
+                logger.error(e1);
+            }
+
+        }
+    }
+
+    private void setTemplateName(String templateName) {
+        this.templateName = templateName;
+
+    }
+
+    private String getTemplateName() {
+        return this.templateName;
+    }
+
+    private Map getReportParameters() {
+
+        return map;
+    }
+
+    private RepositoryReportUnit getReportUnit() {
+        ResourceDescriptor rd = new ResourceDescriptor();
+
+        rd.setName(this.getTemplateName());
+        rd.setParentFolder("/Reports/" + getSite().getName() + "/emailTemplates");
+        rd.setUriString(rd.getParentFolder() + "/" + rd.getName());
+        rd.setWsType(ResourceDescriptor.TYPE_REPORTUNIT);
+        List<ResourceProperty> p = new ArrayList<ResourceProperty>();
+
+        Iterator it = map.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry me = (Map.Entry) it.next();
+
+            ResourceProperty rp = new ResourceProperty(me.getKey().toString(), me.getValue().toString());
+            p.add(rp);
+        }
+
+        rd.setParameters(p);
+
+        return new RepositoryReportUnit(getServer(), rd);
+    }
+
+    private JServer getServer() {
+        return jserver;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
+    public Log getLogger() {
+        return logger;
+    }
+
+    public Site getSite() {
+        return site;
+    }
+
+    public void setSite(Site site) {
+        this.site = site;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public CommunicationHistoryService getCommunicationHistoryService() {
+        return communicationHistoryService;
+    }
+
+    public void setCommunicationHistoryService(
+            CommunicationHistoryService communicationHistoryService) {
+        this.communicationHistoryService = communicationHistoryService;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext)
+            throws BeansException {
+        this.applicationContext = applicationContext;
+
+    }
 }

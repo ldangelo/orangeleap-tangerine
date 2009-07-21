ab7f4d627bf3dc408d0a7397bf34b445032b47e4
ldangelo <ldangelo@0f581344-5646-3a48-8ded-616bfea576c6>
2009-07-21 21:58:20 +0000

    added copyright
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@6941 0f581344-5646-3a48-8ded-616bfea576c6

--- src/com/orangeleap/tangerine/util/TableLoggerImpl.java
+++ src/com/orangeleap/tangerine/util/TableLoggerImpl.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2009. Orange Leap Inc. Active Constituent
+ * Relationship Management Platform.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.orangeleap.tangerine.util;
 
 import com.orangeleap.tangerine.service.ErrorLogService;
@@ -9,9 +27,9 @@ import javax.annotation.Resource;
 
 public class TableLoggerImpl implements TableLogger, ApplicationContextAware, java.io.Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private ApplicationContext applicationContext;
+    private ApplicationContext applicationContext;
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
@@ -21,9 +39,9 @@ public class TableLoggerImpl implements TableLogger, ApplicationContextAware, ja
 
     @Resource(name = "errorLogService")
     private ErrorLogService errorLogService;
-    
+
     public void logToTable(String loglevel, Object o, Throwable t) {
-        errorLogService.addErrorMessage(loglevel+": " + o + (t == null ? "" : ": " + t.getMessage()), "");
+        errorLogService.addErrorMessage(loglevel + ": " + o + (t == null ? "" : ": " + t.getMessage()), "");
     }
 
 }

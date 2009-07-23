bca88ac9941080417929d5c2bab4e4f3c22699ad
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-07-23 14:17:13 +0000

    used to run test as junit test
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@6979 0f581344-5646-3a48-8ded-616bfea576c6

---
+++ test/com/orangeleap/tangerine/test/SwitchTest.java
@@ -0,0 +1,20 @@
+// Can be used for running as JUnit tests
+// Add config and test.properties folders to Junit classpath.
+package com.orangeleap.tangerine.test;
+
+import org.apache.commons.logging.Log;
+import org.springframework.test.AbstractTransactionalDataSourceSpringContextTests;
+
+import com.orangeleap.tangerine.util.OLLogger;
+
+public class SwitchTest extends AbstractTransactionalDataSourceSpringContextTests {
+
+	/** Logger for this class and subclasses */
+    protected final Log logger = OLLogger.getLog(getClass());
+
+	@Override
+ 	protected String[] getConfigLocations() {
+		return new String[]{"test-applicationContext.xml"};
+ 	}
+	
+}

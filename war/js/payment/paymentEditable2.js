55e6ab6136a361a1445843c1c9e611c0d83c7b63
alo <alo@0f581344-5646-3a48-8ded-616bfea576c6>
2009-07-27 19:50:04 +0000

    TANGERINE-816: fixed bug where previously entered pay source info was not redisplayed
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@7047 0f581344-5646-3a48-8ded-616bfea576c6

--- war/js/payment/paymentEditable2.js
+++ war/js/payment/paymentEditable2.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
 	$("#paymentType").bind("change", function() {
 		PaymentEditable.filterPaymentTypes($(this), false);
 	});	
-	$("#ach-paymentSource-tangDot-id").bind("change", function() {
+	$("#ach-paymentSource-td-id").bind("change", function() {
 		PaymentEditable.filterAchPaymentSources($(this), false);
 	});
-	$("#creditCard-paymentSource-tangDot-id").bind("change", function() {
+	$("#creditCard-paymentSource-td-id").bind("change", function() {
 		PaymentEditable.filterCreditCardPaymentSources($(this), false);
 	});
 	PaymentEditable.filterPaymentTypes($("#paymentType"), true);
@@ -20,20 +20,20 @@ var PaymentEditable = {
 			$("#" + PaymentEditable.commandObject + "_creditCard").hide();
 		} 
 		else if (paymentTypeVal == "Credit Card") {
-			$("#ach-paymentSource-tangDot-id").hide();
+			$("#ach-paymentSource-td-id").hide();
 			$("#" + PaymentEditable.commandObject + "_ach").hide();
 			$("#" + PaymentEditable.commandObject + "_creditCard").show();
 
-			PaymentEditable.filterCreditCardPaymentSources($("#creditCard-paymentSource-tangDot-id"), isLoad);
-			$("#creditCard-paymentSource-tangDot-id").show();
+			PaymentEditable.filterCreditCardPaymentSources($("#creditCard-paymentSource-td-id"), isLoad);
+			$("#creditCard-paymentSource-td-id").show();
 		}
 		else if (paymentTypeVal == "ACH") {
-			$("#creditCard-paymentSource-tangDot-id").hide();
+			$("#creditCard-paymentSource-td-id").hide();
 			$("#" + PaymentEditable.commandObject + "_creditCard").hide();
 			$("#" + PaymentEditable.commandObject + "_ach").show();
 
-			PaymentEditable.filterAchPaymentSources($("#ach-paymentSource-tangDot-id"), isLoad);
-			$("#ach-paymentSource-tangDot-id").show();
+			PaymentEditable.filterAchPaymentSources($("#ach-paymentSource-td-id"), isLoad);
+			$("#ach-paymentSource-td-id").show();
 		}
 		else {
 			// Other type, hide everything else TODO:
@@ -45,7 +45,7 @@ var PaymentEditable = {
 	filterAchPaymentSources: function($achPaymentSourceElem, isLoad) {
 		var $option = $achPaymentSourceElem.find('option:selected');
 		var optionVal = $option.val();
-		$("#paymentSource-tangDot-id").val(optionVal);
+		$("#paymentSource-td-id").val(optionVal);
 		if (optionVal == "0") { // 0 is new
 			$("li:has(.ea-existingAch)").hide();
 			$("li:has(.ea-newAch)").show();
@@ -53,16 +53,14 @@ var PaymentEditable = {
 		else if (PaymentEditable.hasId(optionVal)) {
 			$("li:has(.ea-newAch)").hide();
 			$("li:has(.ea-existingAch)").show();
-			if (!isLoad) {
-				PaymentEditable.populatePaymentSourceAttributes($option);
-			}
+			PaymentEditable.populatePaymentSourceAttributes($option, isLoad);
 		}
 	},
 		
 	filterCreditCardPaymentSources: function($creditCardPaymentSourceElem, isLoad) {
 		var $option = $creditCardPaymentSourceElem.find('option:selected');
 		var optionVal = $option.val(); 
-		$("#paymentSource-tangDot-id").val(optionVal);
+		$("#paymentSource-td-id").val(optionVal);
 
 		if (optionVal == "0") {
 			$("li:has(.ea-existingCredit)").hide();
@@ -71,50 +69,51 @@ var PaymentEditable = {
 		else if (PaymentEditable.hasId(optionVal)) {
 			$("li:has(.ea-newCredit)").hide();
 			$("li:has(.ea-existingCredit)").show();
-			if (!isLoad) {
-				PaymentEditable.populatePaymentSourceAttributes($option);
-			}
+			PaymentEditable.populatePaymentSourceAttributes($option, isLoad);
 		}
 	},
 		
-	populatePaymentSourceAttributes: function($option) {
+	populatePaymentSourceAttributes: function($option, isLoad) {
 		if ($option.length && PaymentEditable.hasId($option.val())) {
-			var addressId = $option.attr("address");
-			var phoneId = $option.attr("phone");
-			
-			var $selectAddress = Picklist.setSelectedAddressPhoneByValue($("select#address-tangDot-id", "form"), addressId);
-			var $selectPhone = Picklist.setSelectedAddressPhoneByValue($("select#phone-tangDot-id", "form"), phoneId);
-			
+
 			// ACH
 			var achholder = $option.attr("achholder");
 			if (achholder) {
-				$("#" + PaymentEditable.commandObject + "-paymentSource-achHolderNameReadOnly div#paymentSource-tangDot-achHolderName", "form").text(achholder);
+				$("#" + PaymentEditable.commandObject + "-paymentSource-achHolderNameReadOnly div#paymentSource-td-achHolderName", "form").text(achholder);
 			}
 			var routing = $option.attr("routing");
 			if (routing) {
-				$("#" + PaymentEditable.commandObject + "-paymentSource-achRoutingNumberReadOnly div#paymentSource-tangDot-achRoutingNumberDisplay", "form").text(routing);
+				$("#" + PaymentEditable.commandObject + "-paymentSource-achRoutingNumberReadOnly div#paymentSource-td-achRoutingNumberDisplay", "form").text(routing);
 			}
 			var acct = $option.attr("acct");
 			if (acct) {
-				$("#" + PaymentEditable.commandObject + "-paymentSource-achAccountNumberReadOnly div#paymentSource-tangDot-achAccountNumberDisplay", "form").text(acct);
+				$("#" + PaymentEditable.commandObject + "-paymentSource-achAccountNumberReadOnly div#paymentSource-td-achAccountNumberDisplay", "form").text(acct);
 			}
-			
+
 			// Credit Card
 			var cardholder = $option.attr("cardholder");
 			if (cardholder) {
-				$("#" + PaymentEditable.commandObject + "-paymentSource-creditCardHolderNameReadOnly div#paymentSource-tangDot-creditCardHolderName", "form").text(cardholder);
+				$("#" + PaymentEditable.commandObject + "-paymentSource-creditCardHolderNameReadOnly div#paymentSource-td-creditCardHolderName", "form").text(cardholder);
 			}
 			var cardType = $option.attr("cardType");
 			if (cardType) {
-				$("#" + PaymentEditable.commandObject + "-paymentSource-creditCardTypeReadOnly div#paymentSource-tangDot-creditCardType", "form").text(cardType);
+				$("#" + PaymentEditable.commandObject + "-paymentSource-creditCardTypeReadOnly div#paymentSource-td-creditCardType", "form").text(cardType);
 			}
 			var number = $option.attr("number");
 			if (number) {
-				$("#" + PaymentEditable.commandObject + "-paymentSource-creditCardNumberReadOnly div#paymentSource-tangDot-creditCardNumberDisplay", "form").text(number);
+				$("#" + PaymentEditable.commandObject + "-paymentSource-creditCardNumberReadOnly div#paymentSource-td-creditCardNumberDisplay", "form").text(number);
 			}
 			var exp = $option.attr("exp");
 			if (exp) {
-				$("#" + PaymentEditable.commandObject + "-paymentSource-creditCardExpirationDisplay div#paymentSource-tangDot-creditCardExpiration", "form").text(exp);
+				$("#" + PaymentEditable.commandObject + "-paymentSource-creditCardExpirationDisplay div#paymentSource-td-creditCardExpiration", "form").text(exp);
+			}
+
+			if (!isLoad) {
+				var addressId = $option.attr("address");
+				var phoneId = $option.attr("phone");
+
+				var $selectAddress = Picklist.setSelectedAddressPhoneByValue($("select#address-td-id", "form"), addressId);
+				var $selectPhone = Picklist.setSelectedAddressPhoneByValue($("select#phone-td-id", "form"), phoneId);
 			}
 		}
 		if ($selectAddress) {

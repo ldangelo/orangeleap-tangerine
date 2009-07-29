35c751ab2de4761287f43785f350d757d11b6600
alo <alo@0f581344-5646-3a48-8ded-616bfea576c6>
2009-07-29 23:03:32 +0000

    TANGERINE-816:  gift in kind entity defaults
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@7160 0f581344-5646-3a48-8ded-616bfea576c6

--- db/prod/domain/entityDefault.sql
+++ db/prod/domain/entityDefault.sql
@@ -7,14 +7,14 @@ values ('permanent', 'activationStatus', 'email');
 insert into ENTITY_DEFAULT (DEFAULT_VALUE, ENTITY_FIELD_NAME, ENTITY_TYPE)
 values ('permanent', 'activationStatus', 'phone');
 
-insert into ENTITY_DEFAULT (DEFAULT_VALUE, CONDITION_FIELD_NAME, CONDITION_VALUE, ENTITY_FIELD_NAME, ENTITY_TYPE)
-values ('now:', 'activationStatus', 'permanent', 'effectiveDate', 'address');
+insert into ENTITY_DEFAULT (DEFAULT_VALUE, CONDITION_EXP, ENTITY_FIELD_NAME, ENTITY_TYPE)
+values ('now:', 'activationStatus == ''permanent''', 'effectiveDate', 'address');
 
-insert into ENTITY_DEFAULT (DEFAULT_VALUE, CONDITION_FIELD_NAME, CONDITION_VALUE, ENTITY_FIELD_NAME, ENTITY_TYPE)
-values ('now:', 'activationStatus', 'permanent', 'effectiveDate', 'phone');
+insert into ENTITY_DEFAULT (DEFAULT_VALUE, CONDITION_EXP, ENTITY_FIELD_NAME, ENTITY_TYPE)
+values ('now:', 'activationStatus == ''permanent''', 'effectiveDate', 'phone');
 
-insert into ENTITY_DEFAULT (DEFAULT_VALUE, CONDITION_FIELD_NAME, CONDITION_VALUE, ENTITY_FIELD_NAME, ENTITY_TYPE)
-values ('now:', 'activationStatus', 'permanent', 'effectiveDate', 'email');
+insert into ENTITY_DEFAULT (DEFAULT_VALUE, CONDITION_EXP, ENTITY_FIELD_NAME, ENTITY_TYPE)
+values ('now:', 'activationStatus == ''permanent''', 'effectiveDate', 'email');
 
 insert into ENTITY_DEFAULT (DEFAULT_VALUE, ENTITY_FIELD_NAME, ENTITY_TYPE)
 values ('bean:constituent.firstLast', 'adjustedPaymentTo', 'adjustedGift');
@@ -37,14 +37,17 @@ values ('1', 'customFieldMap[maximumReminders]', 'pledge');
 insert into ENTITY_DEFAULT (DEFAULT_VALUE, ENTITY_FIELD_NAME, ENTITY_TYPE)
 values ('0', 'customFieldMap[reminderInterval]', 'pledge');
 
-insert into ENTITY_DEFAULT (DEFAULT_VALUE, CONDITION_FIELD_NAME, CONDITION_VALUE, ENTITY_FIELD_NAME, ENTITY_TYPE)
-values ('unknown', 'constituentType', 'organization', 'customFieldMap[organization.eligibility]', 'constituent');
+insert into ENTITY_DEFAULT (DEFAULT_VALUE, CONDITION_EXP, ENTITY_FIELD_NAME, ENTITY_TYPE)
+values ('unknown', 'constituentType == ''organization''', 'customFieldMap[organization.eligibility]', 'constituent');
 
 insert into ENTITY_DEFAULT (DEFAULT_VALUE, ENTITY_FIELD_NAME, ENTITY_TYPE)
 values ('true', 'customFieldMap[taxDeductible]', 'distributionLine');
 
 insert into ENTITY_DEFAULT (DEFAULT_VALUE, ENTITY_FIELD_NAME, ENTITY_TYPE)
 values ('bean:constituent.recognitionName', 'customFieldMap[recognitionName]', 'distributionLine');
+
+insert into ENTITY_DEFAULT (DEFAULT_VALUE, ENTITY_FIELD_NAME, ENTITY_TYPE)
+values ('bean:constituent.recognitionName', 'recognitionName', 'giftInKind');
 
 insert into ENTITY_DEFAULT (DEFAULT_VALUE, ENTITY_FIELD_NAME, ENTITY_TYPE)
 values ('bean:constituent.firstLast', 'creditCardHolderName', 'paymentSource');

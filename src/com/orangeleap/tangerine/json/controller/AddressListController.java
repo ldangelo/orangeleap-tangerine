d42523cb336cb8482f6b51b9b56ad8b1ed2b7b47
alo <alo@0f581344-5646-3a48-8ded-616bfea576c6>
2009-08-22 03:39:24 +0000

    TANGERINE-816: code cleanup
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@7508 0f581344-5646-3a48-8ded-616bfea576c6

--- src/com/orangeleap/tangerine/json/controller/AddressListController.java
+++ src/com/orangeleap/tangerine/json/controller/AddressListController.java
@@ -30,7 +30,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -52,41 +51,8 @@ public class AddressListController extends TangerineJsonListController {
 
         int count = addressService.readCountByConstituentId(constituentId);
 
-//        List<Map> list = new ArrayList<Map>();
-//        String constituentId = request.getParameter("constituentId");
-//        String sortField = request.getParameter("sort");
-//        boolean isAsc = "ASC".equals(request.getParameter("dir"));
-//
-//        List<Address> addresses = addressService.readByConstituentId(Long.parseLong(constituentId));
-//
-//        for (Address address : addresses) {
-//            list.add(addressToMap(address, constituentId));
-//        }
-//        int count = addresses.size();
-//
-//        MutableSortDefinition sortDef = new MutableSortDefinition(sortField, true, isAsc);
-//        TangerinePagedListHolder pagedListHolder = new TangerinePagedListHolder(list, sortDef);
-//        pagedListHolder.resort();
-
         ModelMap map = new ModelMap("rows", list);
         map.put("totalRows", count);
-
-        return map;
-    }
-
-    private Map<String, Object> addressToMap(Address address, String constituentId) {
-        Map<String, Object> map = new HashMap<String, Object>();
-        map.put("id", address.getId());
-        map.put("constituentId", constituentId);
-        map.put("addressLine1", address.getAddressLine1());
-        map.put("city", address.getCity());
-        map.put("stateProvince", address.getStateProvince());
-        map.put("postalCode", address.getPostalCode());
-        map.put("country", address.getCountry());
-        map.put("receiveCorrespondence", address.isReceiveCorrespondence());
-        map.put("current", addressService.isCurrent(address));
-        map.put("primary", address.isPrimary());
-        map.put("active", !address.isInactive());
 
         return map;
     }

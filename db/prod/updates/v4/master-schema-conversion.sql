42bee5050ce030fecec8f39a3294cebe48808059
eoland <eoland@0f581344-5646-3a48-8ded-616bfea576c6>
2009-09-04 17:54:27 +0000

    Trying to fix problem with svn.
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@7694 0f581344-5646-3a48-8ded-616bfea576c6

--- db/prod/updates/v4/master-schema-conversion.sql
+++
@@ -1,8 +0,0 @@
-/*
-V4 Schema changes
-*********************************************************************
-*/
-
--- TANGERINE-1185
-ALTER TABLE SITE DROP COLUMN MAJOR_DONOR_ACCOUNT_MANAGER_ID, DROP COLUMN FISCAL_YEAR_STARTING_MONTH;
-

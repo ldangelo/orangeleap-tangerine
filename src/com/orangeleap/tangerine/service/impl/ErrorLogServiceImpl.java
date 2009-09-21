df047b7b37eb6ac0ba60372fd34895ecbb96133e
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-09-21 14:21:21 +0000

    prevent missing schema from looping when the error table is missing
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@7967 0f581344-5646-3a48-8ded-616bfea576c6

--- src/com/orangeleap/tangerine/service/impl/ErrorLogServiceImpl.java
+++ src/com/orangeleap/tangerine/service/impl/ErrorLogServiceImpl.java
@@ -21,7 +21,7 @@ package com.orangeleap.tangerine.service.impl;
 import javax.annotation.Resource;
 
 import org.apache.commons.logging.Log;
-import com.orangeleap.tangerine.util.OLLogger;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,18 +29,15 @@ import org.springframework.transaction.annotation.Transactional;
 import com.orangeleap.tangerine.dao.ErrorLogDao;
 import com.orangeleap.tangerine.service.ErrorLogService;
 import com.orangeleap.tangerine.util.TangerineUserHelper;
-import com.orangeleap.tangerine.domain.ErrorLog;
 import com.orangeleap.tangerine.web.common.PaginatedResult;
 import com.orangeleap.tangerine.web.common.SortInfo;
-
-import java.util.List;
 
 @Service("errorLogService")
 @Transactional(propagation = Propagation.REQUIRED)
 public class ErrorLogServiceImpl extends AbstractTangerineService implements ErrorLogService {
 
     /** Logger for this class and subclasses */
-    protected final Log logger = OLLogger.getLog(getClass());
+    protected final Log logger = LogFactory.getLog(ErrorLogServiceImpl.class);
 
     @Resource(name = "errorLogDAO")
     private ErrorLogDao errorLogDao;
@@ -55,7 +52,7 @@ public class ErrorLogServiceImpl extends AbstractTangerineService implements Err
         	Long constituentId = tangerineUserHelper.lookupUserId();
     		errorLogDao.addErrorMessage(message, context, constituentId);
     	} catch (Exception e) {
-    		logger.error("Unable to log error message to ERROR_LOG: "+message, e);
+    		try {logger.error("Unable to log error message to ERROR_LOG: "+message, e);} catch (Exception e1) {}
     	}
     }
 

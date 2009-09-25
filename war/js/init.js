5b4cfbae5fa972f09572868ceac6e4d9ce037c8e
alo <alo@0f581344-5646-3a48-8ded-616bfea576c6>
2009-09-25 13:40:24 +0000

    TANGERINE-1284: disabled grid column/scroll enhancements until firefox cookie bug fixed
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@8059 0f581344-5646-3a48-8ded-616bfea576c6

--- war/js/init.js
+++ war/js/init.js
@@ -3,5 +3,5 @@
 Ext.BLANK_IMAGE_URL = 'js/extjs/resources/images/default/s.gif';
 
 Ext.onReady(function() {
-    Ext.state.Manager.setProvider(new Ext.state.CookieProvider());     
+//    Ext.state.Manager.setProvider(new Ext.state.CookieProvider());     
 });

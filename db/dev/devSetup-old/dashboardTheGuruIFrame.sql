aebf10ef7213a05f5e8071126f5d95709e4028ec
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-10-02 15:06:27 +0000

    example of using an iframe to display the guru reports with CAS
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@8204 0f581344-5646-3a48-8ded-616bfea576c6

---
+++ db/dev/devSetup-old/dashboardTheGuruIFrame.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO company1.DASHBOARD_ITEM (DASHBOARD_ITEM_TYPE, DASHBOARD_ITEM_TITLE, URL, ITEM_ORDER, ROLES, SITE_NAME) VALUES ('IFrame', 'The Guru' , 
+'https://restaging.orangeleap.com/ruletest-jasperserver/flow.html?_flowId=viewReportFlow&reportUnit=/Reports/QA/THEGURU_14&standAlone=true&ParentFolderUri=/Reports/QA'
+, 7, NULL, NULL);
+

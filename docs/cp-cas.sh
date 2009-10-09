d25ff4dba73a936f4ed11096800090e7fe28438e
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-10-09 19:52:55 +0000

    copy over cas files
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@8336 0f581344-5646-3a48-8ded-616bfea576c6

---
+++ docs/cp-cas.sh
@@ -0,0 +1,20 @@
+export PTH=/opt/orangeleap/instance_TEST/webapps
+
+cd $PTH/jasperserver/WEB-INF
+cp applicationContext-security-CAS.xml.txt applicationContext-security.xml
+
+cd $PTH/jasperserver/WEB-INF/jsp
+cp login_welcome-CAS.jsp login_welcome.jsp
+
+cd $PTH/orangeleap
+cp login-CAS.jsp login.jsp
+
+cd $PTH/orangeleap/WEB-INF
+cp applicationContext-security-CAS.xml.txt applicationContext-security.xml
+
+cd $PTH/orangeleap/WEB-INF/jsp/includes
+cp navigation-CAS.jsp navigation.jsp
+
+cd $PTH/orangeleap/WEB-INF/contexts
+cp webservice-context-CAS.xml.txt webservice-context.xml
+

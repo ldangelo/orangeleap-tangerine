d024f2987324b98f536a60181a09f47b044a0b86
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-10-09 22:06:00 +0000

    draft for cas common auth
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@8344 0f581344-5646-3a48-8ded-616bfea576c6

--- src/com/orangeleap/tangerine/security/common/OrangeLeapAuthenticationProvider.java
+++ src/com/orangeleap/tangerine/security/common/OrangeLeapAuthenticationProvider.java
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.orangeleap.tangerine.security.common;
+package com.mpower.security.common;
 
 import java.util.ArrayList;
 import java.util.List;

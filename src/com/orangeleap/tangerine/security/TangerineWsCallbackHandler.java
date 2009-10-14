d7ef013b259d6e53bd15401bbf3a24c6419f4757
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-10-14 15:04:34 +0000

    populate securitycontext for tangerineuserhelper
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@8429 0f581344-5646-3a48-8ded-616bfea576c6

--- src/com/orangeleap/tangerine/security/TangerineWsCallbackHandler.java
+++ src/com/orangeleap/tangerine/security/TangerineWsCallbackHandler.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.UnsupportedCallbackException;
 
+import org.springframework.security.context.SecurityContextHolder;
 import org.springframework.security.providers.UsernamePasswordAuthenticationToken;
 
 import com.orangeleap.tangerine.security.common.OrangeLeapUsernamePasswordLocal;
@@ -24,6 +25,7 @@ public class TangerineWsCallbackHandler extends org.springframework.ws.soap.secu
 		Object obj = OrangeLeapUsernamePasswordLocal.getOrangeLeapAuthInfo().get(OrangeLeapUsernamePasswordLocal.AUTH_TOKEN);
 		if (obj instanceof UsernamePasswordAuthenticationToken) {
 			UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken)obj;
+			SecurityContextHolder.getContext().setAuthentication(token);
 			token.setDetails(new TangerineAuthenticationDetails());
 			tangerineSessionInformationFilter.loadTangerineDetails(token);
 		}

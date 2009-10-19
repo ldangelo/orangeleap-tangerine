53cfa946785104d9b88da49382cc9ee46113a57b
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-10-19 20:29:54 +0000

    use jasper-relative link
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@8533 0f581344-5646-3a48-8ded-616bfea576c6

--- db/dev/devSetup-old/dashboardTheGuruIFrame.sql
+++ db/dev/devSetup-old/dashboardTheGuruIFrame.sql
@@ -1,5 +1,5 @@
 -- report link is to a graph-only report in the guru.
 INSERT INTO company1.DASHBOARD_ITEM (DASHBOARD_ITEM_TYPE, DASHBOARD_ITEM_TITLE, URL, ITEM_ORDER, ROLES, SITE_NAME) VALUES ('Guru', 'Constituent Type Report' , 
-'https://restaging.orangeleap.com/ruletest-jasperserver/flow.html?_flowId=viewReportFlow&standAlone=true&reportUnit=/Reports/company1/myreport2'
+'/flow.html?_flowId=viewReportFlow&standAlone=true&reportUnit=/Reports/company1/myreport2'
 , 7, NULL, NULL);
 

1664371ca5fb03c5d846f84849b14c2584d44773
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-10-27 22:20:38 +0000

    example
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@8740 0f581344-5646-3a48-8ded-616bfea576c6

--- db/dev/devSetup-old/dashboardTheGuruIFrame.sql
+++ db/dev/devSetup-old/dashboardTheGuruIFrame.sql
@@ -1,5 +1,5 @@
 -- report link is to a graph-only report in the guru.
 INSERT INTO company1.DASHBOARD_ITEM (DASHBOARD_ITEM_TYPE, DASHBOARD_ITEM_TITLE, URL, ITEM_ORDER, ROLES, SITE_NAME) VALUES ('Guru', 'Constituent Type Report' , 
-'flow.html?_flowId=viewReportFlow&standAlone=true&reportUnit=/Reports/company1/myreport2'
+'flow.html?_flowId=viewReportFlow&reportUnit=/Reports/company1/Content_files/test-graph-only-guru-report&standAlone=true&ParentFolderUri=/Reports/company1/Content_files'
 , 7, NULL, 'company1');
 

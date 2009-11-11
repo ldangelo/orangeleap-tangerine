09d13fe111a5d77f04924feefcc752d772fe0380
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-11-11 20:15:28 +0000

    update to v7
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@8939 0f581344-5646-3a48-8ded-616bfea576c6

---
+++ db/prod/updates/v7/master-data-conversion.sql
@@ -0,0 +1,27 @@
+/*
+SQLyog Job Agent Version 8.14 n2 Copyright(c) Webyog Softworks Pvt. Ltd. All Rights Reserved.
+
+MySQL - 5.1.35-community
+*********************************************************************
+*/
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+/* SYNC DB : company1 */
+SET AUTOCOMMIT = 0;
+
+
+/* Schema versions now match release versions  */
+UPDATE VERSION SET SCHEMA_MAJOR_VERSION = 7 WHERE COMPONENT_ID = 'ORANGE' AND COMPONENT_DESC = 'Orange Leap';
+
+
+
+
+COMMIT;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+

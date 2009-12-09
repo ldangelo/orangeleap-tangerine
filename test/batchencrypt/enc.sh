cece04745bb525eafb5f6f8c9765a0c246288cbb
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-12-09 16:42:57 +0000

    add logging jar
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@9286 0f581344-5646-3a48-8ded-616bfea576c6

--- test/batchencrypt/enc.sh
+++ test/batchencrypt/enc.sh
@@ -1,3 +1,3 @@
 export LIB=/opt/orangeleap/instance_DEV/webapps/orangeleap/WEB-INF/lib
-java -cp "$LIB/commons-codec-1.3.jar:$LIB/commons-io-1.2.jar:$LIB/tangerine.jar" com.orangeleap.tangerine.util.AESEncrypt $1 $2 $3
+java -cp "$LIB/commons-logging-1.1.1.jar:$LIB/commons-codec-1.3.jar:$LIB/commons-io-1.2.jar:$LIB/tangerine.jar" com.orangeleap.tangerine.util.AESEncrypt $1 $2 $3
 

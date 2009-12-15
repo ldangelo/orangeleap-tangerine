74ff51693104214e9e307e7b2ebeee509df58ec6
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2009-12-15 22:43:23 +0000

    add event types
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@9362 0f581344-5646-3a48-8ded-616bfea576c6

--- src/com/orangeleap/tangerine/type/RuleEventNameType.java
+++ src/com/orangeleap/tangerine/type/RuleEventNameType.java
@@ -2,10 +2,15 @@ package com.orangeleap.tangerine.type;
 
 public enum RuleEventNameType {
 	
-	GIFT_SAVE("gift-save"),
 	CONSTITUENT_SAVE("constituent-save"),
+	GIFT_SAVE("gift-save"),
 	TOUCHPOINT_SAVE("touchpoint-save"),
-	SCHEDULED("scheduled")
+	EMAIL("email"),
+	PAYMENT_PROCESSING("payment-processing"),
+	SCHEDULED_DAILY("scheduled-daily"),
+	SCHEDULED_WEEKLY("scheduled-weekly"),
+	SCHEDULED_MONTHLY("scheduled-monthly"),
+	SCHEDULED_ONE_TIME("scheduled-one-time")
 	;
 	
 	private String type;

fc1d99dbd0ce578e920cefaf1660da4f05d36897
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2010-01-21 20:15:16 +0000

    add SET_CONSTITUENT_FLAGS
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@9795 0f581344-5646-3a48-8ded-616bfea576c6

--- db/prod/tangerine-functions.sql
+++ db/prod/tangerine-functions.sql
@@ -227,11 +227,54 @@ CREATE DEFINER=`root`@`localhost` FUNCTION `SET_CONSTITUENT_FLAGS`()
         RETURNS int
 BEGIN
 
-        -- TODO set or unset flags for items that can change due to passage of time without any transactions occurring, based on site options.
-        -- Update CONSTITUENT.UPDATE_DATE for any changed records.
-                
-        RETURN 0;
+        -- Set or unset flags for items that can change due to passage of time without any transactions occurring, based on site options.
+        -- Update CONSTITUENT.UPDATE_DATE for any changed records to allow nightly rules to fire on those records.
+
+	
+	  DECLARE cutOffDate datetime default NULL;
+	  DECLARE done INT DEFAULT 0;
+	  DECLARE cutoffAmt VARCHAR(255) default NULL;
+	  DECLARE constituentId, cnt INT;
+	  DECLARE recentGift char(1);
+	  DECLARE cur1 CURSOR FOR SELECT CONSTITUENT_ID, RECENT_GIFT FROM CONSTITUENT;
+	  DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+	  
+	  select OPTION_VALUE from SITE_OPTION WHERE OPTION_NAME = 'lapsed.donor.days' INTO cutoffAmt;
+	  IF cutoffAmt is not null THEN
+		  SET cutOffDate = DATE_SUB(now(), INTERVAL cutoffAmt DAY); 
+	  END IF;
+	  select OPTION_VALUE from SITE_OPTION WHERE OPTION_NAME = 'lapsed.donor.months' INTO cutoffAmt;
+	  IF cutoffAmt is not null THEN
+		  SET cutOffDate = DATE_SUB(now(), INTERVAL cutoffAmt MONTH); 
+	  END IF;
+	  
+	  IF cutOffDate is NULL THEN
+	      RETURN 1;
+	  END IF;
+	  
+	
+	  OPEN cur1;
+	
+	  REPEAT
+	    FETCH cur1 INTO constituentId, recentGift;
+	    IF NOT done THEN
+	       select count(*) INTO cnt from GIFT g where g.CONSTITUENT_ID = id and g.DONATION_DATE >= cutOffDate;
+	       IF cnt > 0 THEN
+	          IF recentGift = '0' THEN
+		          UPDATE CONSTITUENT set RECENT_GIFT = '1', UPDATE_DATE = now() where CONSTITUENT_ID = constituentId;
+		      END IF;
+	       ELSE
+	          IF recentGift = '1' THEN
+		          UPDATE CONSTITUENT set RECENT_GIFT = '0', UPDATE_DATE = now() where CONSTITUENT_ID = constituentId;
+		      END IF;
+	       END IF;
+	    END IF;
+	  UNTIL done END REPEAT;
+	
+	  CLOSE cur1;
+	
+	
+      RETURN 0;
         
 END$$
-
 

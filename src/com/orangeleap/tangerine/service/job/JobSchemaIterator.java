ff9540957963875b191fadec9a264357ba1e67ca
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2010-02-02 15:48:13 +0000

    format log message
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@9989 0f581344-5646-3a48-8ded-616bfea576c6

--- src/com/orangeleap/tangerine/service/job/JobSchemaIterator.java
+++ src/com/orangeleap/tangerine/service/job/JobSchemaIterator.java
@@ -107,7 +107,7 @@ public class JobSchemaIterator extends QuartzJobBean {
             		}
             		
 	                schemaService.setSchema(schema);  //  sets tangerine user helper with site name for TangerineDatasource to read.
-	                logger.info("Processing jobs for "+schema); // Must log after changing schema context
+	                logger.info("Processing jobs for [" + schema + "] ---------------------------------------------------");  // Must log after changing schema context
 	                long t = System.currentTimeMillis();
 	                executeInternalForSchema(context, applicationContext);
 	                logger.info("Jobs for schema " + schema + " took " + (System.currentTimeMillis() - t)/1000.0f/60f + " minutes.");

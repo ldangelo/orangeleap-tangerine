c49ac282bc40a818988753fed96b10f26fc659fb
dmeany <dmeany@0f581344-5646-3a48-8ded-616bfea576c6>
2010-02-11 21:44:44 +0000

    TANGERINE-1450 Background Entry screen drafts
    git-svn-id: svn://svn.orangeleap.com/tangerine/tangerine/trunk@10201 0f581344-5646-3a48-8ded-616bfea576c6

--- db/prod/domain/backgroundDomain.sql
+++ db/prod/domain/backgroundDomain.sql
@@ -1,5 +1,5 @@
 INSERT INTO FIELD_DEFINITION (FIELD_DEFINITION_ID, ENTITY_TYPE, FIELD_NAME, DEFAULT_LABEL, FIELD_TYPE) VALUES ('background.id', 'background', 'id', 'Reference Number', 'TEXT');
-INSERT INTO FIELD_DEFINITION (FIELD_DEFINITION_ID, ENTITY_TYPE, FIELD_NAME, DEFAULT_LABEL, FIELD_TYPE) VALUES ('background.customFieldMap[entryType]', 'background', 'customFieldMap[entryType]', 'Entry Type', 'PICKLIST');
+INSERT INTO FIELD_DEFINITION (FIELD_DEFINITION_ID, ENTITY_TYPE, FIELD_NAME, DEFAULT_LABEL, FIELD_TYPE) VALUES ('background.customFieldMap[backgroundEntryType]', 'background', 'customFieldMap[backgroundEntryType]', 'Entry Type', 'PICKLIST');
 INSERT INTO FIELD_DEFINITION (FIELD_DEFINITION_ID, ENTITY_TYPE, FIELD_NAME, DEFAULT_LABEL, FIELD_TYPE) VALUES ('background.customFieldMap[title]', 'background', 'customFieldMap[title]', 'Title', 'TEXT');
 INSERT INTO FIELD_DEFINITION (FIELD_DEFINITION_ID, ENTITY_TYPE, FIELD_NAME, DEFAULT_LABEL, FIELD_TYPE) VALUES ('background.comments', 'background', 'comments', 'Comments', 'LONG_TEXT');
 
